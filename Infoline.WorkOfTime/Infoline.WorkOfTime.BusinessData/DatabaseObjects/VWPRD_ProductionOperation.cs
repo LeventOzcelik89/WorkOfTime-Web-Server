03f06f5f6dcf5d968bb61249815b86c5e5e7ebb5
Ahmet Undemir <ahmet.undemir@infoline-tr.com>
2021-09-07 08:33:54 +0300

    ProductionOperation view'ına ProductionStageId_Title alanı eklendi.

--- Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWPRD_ProductionOperation.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWPRD_ProductionOperation.cs
@@ -12,6 +12,7 @@ namespace Infoline.WorkOfTime.BusinessData
         public Guid? productionStageId { get; set;}
         public string createdby_Title { get; set;}
         public string changedby_Title { get; set;}
+        public string productionStageId_Title { get; set;}
         public string status_Title { get; set;}
     }
 }
