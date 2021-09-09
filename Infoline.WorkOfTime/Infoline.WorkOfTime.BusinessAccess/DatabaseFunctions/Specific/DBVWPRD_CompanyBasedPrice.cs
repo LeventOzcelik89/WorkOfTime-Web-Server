79c14be4169f85abcee25b8a8945d756f8ae671f
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2021-09-09 10:41:10 +0300

    Companybasedprice için enumların belirlenmesi

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/DatabaseFunctions/Specific/DBVWPRD_CompanyBasedPrice.cs
@@ -0,0 +1,36 @@
+﻿using Infoline.WorkOfTime.BusinessData;
+using System.ComponentModel;
+namespace Infoline.WorkOfTime.BusinessAccess
+{
+    [EnumInfo(typeof(VWPRD_CompanyBasedPrice), "EnumPRD_CompanyBasedPriceSellingType")]
+    public enum EnumPRD_CompanyBasedPriceSellingType
+    {
+        [Description("Genel")]
+        Genel = 0,
+        [Description("Peşin")]
+        Peşin,
+        [Description("Taksitli")]
+        Taksitli,
+        [Description("Vadeli")]
+        Vadeli
+    }
+    [EnumInfo(typeof(VWPRD_CompanyBasedPrice), "EnumPRD_CompanyBasedPriceType")]
+    public enum EnumPRD_CompanyBasedPriceType
+    {
+        [Description("Oran")]
+        Oran = 0,
+        [Description("Fiyat")]
+        Fiyat,
+    }
+    [EnumInfo(typeof(VWPRD_CompanyBasedPrice), "EnumPRD_CompanyBasedPriceConditionType")]
+    public enum EnumPRD_CompanyBasedPriceConditionType
+    {
+        [Description("Genel")]
+        Oran = 0,
+        [Description("Satış Fiyatına Göre")]
+        Fiyat
+    }
+    partial class WorkOfTimeDatabase
+    {
+    }
+}
