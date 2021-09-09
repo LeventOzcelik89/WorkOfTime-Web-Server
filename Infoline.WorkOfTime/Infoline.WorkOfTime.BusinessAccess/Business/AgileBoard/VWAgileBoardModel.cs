b746aa25ac0a26867bbfafe53ac887b89af0f3c6
Levent OZCELIK <levent.ozcelik@infoline-tr.com>
2021-09-09 10:48:31 +0300

    AgileBoard geliştirmeleri

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/Business/AgileBoard/VWAgileBoardModel.cs
@@ -0,0 +1,144 @@
+﻿using Infoline.Framework.Database;
+using Infoline.WorkOfTime.BusinessData;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Infoline.WorkOfTime.BusinessAccess
+{
+
+    public class VWAgileBoardModel : VWSH_AgileBoards
+    {
+        private WorkOfTimeDatabase db { get; set; }
+        private DbTransaction trans { get; set; }
+
+        public bool IsUpdate { get; set; }
+
+        public VWAgileBoardModel Load(Guid? userId = null)
+        {
+            this.db = this.db ?? new WorkOfTimeDatabase();
+            var task = db.GetVWFTM_TaskById(this.id);
+            if (task != null)
+            {
+                this.B_EntityDataCopyForMaterial(task, true);
+                this.IsUpdate = true;
+            }
+            else
+            {
+
+            }
+
+            return this;
+        }
+
+        public ResultStatus Save(Guid? userId, HttpRequestBase request = null, DbTransaction trans = null)
+        {
+
+            db = db ?? new WorkOfTimeDatabase();
+            var product = db.GetVWSH_AgileBoardsById(this.id);
+            var rs = new ResultStatus { result = true };
+
+            if (product == null)
+            {
+                this.createdby = userId;
+                this.created = DateTime.Now;
+                rs = Insert(trans);
+            }
+            else
+            {
+                this.changedby = userId;
+                this.changed = DateTime.Now;
+                rs = Update(trans);
+            }
+
+            if (rs.result)
+            {
+                if (request != null)
+                {
+                    new FileUploadSave(request, this.id).SaveAs();
+                }
+            }
+
+            return rs;
+        }
+
+        private ResultStatus Insert(DbTransaction trans = null)
+        {
+            db = db ?? new WorkOfTimeDatabase();
+            var transaction = trans ?? db.BeginTransaction();
+            var agile = this.B_ConvertType<SH_AgileBoards>();
+
+            var rs = db.InsertSH_AgileBoards(agile, transaction);
+
+            if (rs.result == true)
+            {
+                if (trans == null) transaction.Commit();
+                return new ResultStatus { result = true, message = "Agile Board başarıyla kaydedildi." };
+            }
+            else
+            {
+                if (trans == null) transaction.Rollback();
+                return new ResultStatus { result = false, message = rs.message };
+            }
+        }
+
+        private ResultStatus Update(DbTransaction trans = null)
+        {
+            db = db ?? new WorkOfTimeDatabase();
+            var transaction = trans ?? db.BeginTransaction();
+            var agile = new PRD_Product().B_EntityDataCopyForMaterial(this, true);
+
+            var rs = db.UpdatePRD_Product(agile, true, transaction);
+
+            if (rs.result == true)
+            {
+                if (trans == null) transaction.Commit();
+                return new ResultStatus { result = true, message = "Agile Board güncelleme işlemi başarıyla tamamlandı." };
+            }
+            else
+            {
+                if (trans == null) transaction.Rollback();
+                return new ResultStatus { result = false, message = rs.message };
+            }
+        }
+
+        public ResultStatus Delete(DbTransaction trans = null)
+        {
+            db = db ?? new WorkOfTimeDatabase();
+            var agile = db.GetSH_AgileBoardsById(this.id);
+            if (agile == null)
+            {
+                return new ResultStatus { result = false, message = "Agile Board zaten silinmiş." };
+            }
+
+            var dbres = new ResultStatus { result = true };
+            var transaction = trans ?? db.BeginTransaction();
+
+            dbres &= db.DeleteSH_AgileBoards(agile, trans);
+
+            if (dbres.result == true)
+            {
+                if (trans == null) transaction.Commit();
+                return new ResultStatus
+                {
+                    result = true,
+                    message = "Agile Board silme işlemi başarıyla tamamlandı."
+                };
+            }
+            else
+            {
+                if (trans == null) transaction.Rollback();
+                return new ResultStatus
+                {
+                    result = false,
+                    message = dbres.message
+                };
+            }
+        }
+
+    }
+}
