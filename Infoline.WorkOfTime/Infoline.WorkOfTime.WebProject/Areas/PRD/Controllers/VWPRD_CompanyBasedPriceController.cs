a3861560416a7a2a998a0dcacbe715f61dadc27b
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2021-09-09 10:30:29 +0300

    Companybasedprice ve companybasedpriceDetail sayfaları ve veritaban dosyaları oluşturuldu

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.WebProject/Areas/PRD/Controllers/VWPRD_CompanyBasedPriceController.cs
@@ -0,0 +1,132 @@
+﻿using Infoline.WorkOfTime.BusinessData;
+using Infoline.WorkOfTime.BusinessAccess;
+using Infoline.Web.Utility;
+using Kendo.Mvc;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Infoline.WorkOfTime.WebProject.Areas.PRD.Controllers
+{
+	public class VWPRD_CompanyBasedPriceController : Controller
+	{
+		public ActionResult Index()
+		{
+		    return View();
+		}
+
+
+		public ContentResult DataSource([DataSourceRequest]DataSourceRequest request)
+		{
+		    var condition = KendoToExpression.Convert(request);
+
+		    var page = request.Page;
+		    request.Filters = new FilterDescriptor[0];
+		    request.Sorts = new SortDescriptor[0];
+		    request.Page = 1;
+		    var db = new WorkOfTimeDatabase();
+		    var data = db.GetVWPRD_CompanyBasedPrice(condition).RemoveGeographies().ToDataSourceResult(request);
+		    data.Total = db.GetVWPRD_CompanyBasedPriceCount(condition.Filter);
+		    return Content(Infoline.Helper.Json.Serialize(data), "application/json");
+		}
+
+
+		public ContentResult DataSourceDropDown([DataSourceRequest]DataSourceRequest request)
+		{
+		    var condition = KendoToExpression.Convert(request);
+
+		    var db = new WorkOfTimeDatabase();
+		    var data = db.GetVWPRD_CompanyBasedPrice(condition);
+		    return Content(Infoline.Helper.Json.Serialize(data), "application/json");
+		}
+
+
+		public ActionResult Detail(Guid id)
+		{
+		    var db = new WorkOfTimeDatabase();
+		    var data = db.GetVWPRD_CompanyBasedPriceById(id);
+		    return View(data);
+		}
+
+
+		public ActionResult Insert()
+		{
+		    var data = new VWPRD_CompanyBasedPrice { id = Guid.NewGuid() };
+		    return View(data);
+		}
+
+
+		[HttpPost, ValidateAntiForgeryToken]
+		public JsonResult Insert(PRD_CompanyBasedPrice item)
+		{
+		    var db = new WorkOfTimeDatabase();
+		    var userStatus = (PageSecurity)Session["userStatus"];
+		    var feedback = new FeedBack();
+		    item.created = DateTime.Now;
+		    item.createdby = userStatus.user.id;
+		    var dbresult = db.InsertPRD_CompanyBasedPrice(item);
+		    var result = new ResultStatusUI
+		    {
+		        Result = dbresult.result,
+		        FeedBack = dbresult.result ? feedback.Success("Kaydetme işlemi başarılı") : feedback.Error("Kaydetme işlemi başarısız")
+		    };
+		
+		    return Json(result, JsonRequestBehavior.AllowGet);
+		}
+
+
+		public ActionResult Update(Guid id)
+		{
+		    var db = new WorkOfTimeDatabase();
+		    var data = db.GetVWPRD_CompanyBasedPriceById(id);
+		    return View(data);
+		}
+
+
+		[HttpPost, ValidateAntiForgeryToken]
+		public JsonResult Update(PRD_CompanyBasedPrice item)
+		{
+		    var db = new WorkOfTimeDatabase();
+		    var userStatus = (PageSecurity)Session["userStatus"];
+		    var feedback = new FeedBack();
+		
+		    item.changed = DateTime.Now;
+		    item.changedby = userStatus.user.id;
+		
+		    var dbresult = db.UpdatePRD_CompanyBasedPrice(item);
+		    var result = new ResultStatusUI
+		    {
+		        Result = dbresult.result,
+		        FeedBack = dbresult.result ? feedback.Success("Güncelleme işlemi başarılı") : feedback.Error("Güncelleme işlemi başarısız")
+		    };
+		
+		    return Json(result, JsonRequestBehavior.AllowGet);
+		}
+
+
+		[HttpPost]
+		public JsonResult Delete(string[] id)
+		{
+		    var db = new WorkOfTimeDatabase();
+		    var feedback = new FeedBack();
+		
+		    var item = id.Select(a => new PRD_CompanyBasedPrice { id = new Guid(a) });
+		
+		    var dbresult = db.BulkDeletePRD_CompanyBasedPrice(item);
+		
+		    var result = new ResultStatusUI
+		    {
+		        Result = dbresult.result,
+		        FeedBack = dbresult.result ? feedback.Success("Silme işlemi başarılı") : feedback.Error("Silme işlemi başarılı")
+		    };
+		
+		    return Json(result, JsonRequestBehavior.AllowGet);
+		}
+
+
+
+	}
+}
