a862835171b0643d32f8482ab251fe01bf87b287
Oğuz Yavuz <oguz.yavuz@infoline-tr.com>
2021-09-30 10:29:03 +0300

    Servis düzenlemesi yapıldı.

--- Infoline.WorkOfTime/Infoline.WorkOfTime.WebService/HandlersSpecific/CRM/VWCRM_Contact.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.WebService/HandlersSpecific/CRM/VWCRM_Contact.cs
@@ -37,6 +37,13 @@ namespace Infoline.WorkOfTime.WebService.HandlersSpecific
             }
         }
 
+        public int GetUserByType(Guid userId)
+        {
+            var db = new WorkOfTimeDatabase();
+            var _shuser = db.GetVWSH_UserById(userId);
+            return _shuser.type.HasValue ? _shuser.type.Value : (int)EnumSH_UserType.MyPerson;
+
+        }
 
         [HandleFunction("VWCRM_Contact/Insert")]
         public void VWCRM_ContactInsert(HttpContext context)
@@ -45,6 +52,206 @@ namespace Infoline.WorkOfTime.WebService.HandlersSpecific
             {
                 var model = ParseRequest<VMCRM_ContactModel>(context);
                 var userId = CallContext.Current.UserId;
+                var db = new WorkOfTimeDatabase();
+                var feedback = new FeedBack();
+                model.created = DateTime.Now;
+                model.createdby = CallContext.Current.UserId;
+
+                if (!model.RelationId.HasValue)
+                {
+                    if (model.PresentationId == null)
+                    {
+                        RenderResponse(context, new ResultStatus { result = true, message = "Bağlı potansiyel fırsat bulunamadı." });
+                        return;
+                    }
+                }
+
+                if (model.RelationId.HasValue)
+                {
+                    var cmp = db.GetCMP_CompanyById(model.RelationId.Value);
+                    if (cmp != null)
+                    {
+                        model.customerId = model.RelationId;
+                    }
+                    else
+                    {
+                        model.PresentationId = model.RelationId;
+                    }
+                }
+
+
+                var trans = db.BeginTransaction();
+                var dbresult = db.InsertCRM_Contact(new CRM_Contact().B_EntityDataCopyForMaterial(model, true), trans);
+
+                if (model.PresentationId.HasValue)
+                {
+                    dbresult &= db.InsertCRM_PresentationAction(new CRM_PresentationAction
+                    {
+                        created = DateTime.Now,
+                        createdby = CallContext.Current.UserId,
+                        description = "Yeni aktivite/randevu eklendi.",
+                        presentationId = model.PresentationId.Value,
+                        type = (short)EnumCRM_PresentationActionType.YeniAktivite,
+                        contactId = model.id,
+                    }, trans);
+                }
+                dbresult &= db.BulkInsertCRM_ContactUser(model.Users.Select(a => new CRM_ContactUser
+                {
+                    id = Guid.NewGuid(),
+                    created = DateTime.Now,
+                    createdby = a.UserId,
+                    UserId = a.UserId,
+                    ContactId = model.id,
+                    UserType = GetUserByType(a.UserId)
+                }), trans);
+
+                if (model.PresentationStageId != null && model.PresentationId.HasValue)
+                {
+                    var presetation = db.GetCRM_PresentationById(model.PresentationId.Value);
+                    if (presetation.PresentationStageId != model.PresentationStageId)
+                    {
+                        var oldStage = db.GetCRM_ManagerStageById(presetation.PresentationStageId.Value);
+                        var newstage = db.GetCRM_ManagerStageById(model.PresentationStageId.Value);
+                        if (oldStage != null && newstage != null)
+                        {
+                            presetation.changedby = CallContext.Current.UserId;
+                            presetation.changed = DateTime.Now;
+                            presetation.PresentationStageId = model.PresentationStageId;
+                            dbresult &= db.UpdateCRM_Presentation(presetation, false, trans);
+                            dbresult &= db.InsertCRM_PresentationAction(new CRM_PresentationAction
+                            {
+                                created = DateTime.Now,
+                                createdby = CallContext.Current.UserId,
+                                presentationId = model.PresentationId,
+                                color = newstage.color,
+                                type = (short)EnumCRM_PresentationActionType.AsamaGüncelleme,
+                                description = "Aşama Güncellendi.  " + oldStage.Name + " => " + newstage.Name
+                            }, trans);
+                        }
+                    }
+                }
+
+                var hata = "";
+                if (dbresult.result == false)
+                {
+                    trans.Rollback();
+                    RenderResponse(context, new ResultStatus { message = "Aktivite/Randevu tanımlama işlemi başarısız oldu.", result = false });
+                    return;
+                }
+                else
+                {
+                    trans.Commit();
+                }
+
+                try
+                {
+
+
+                    var oldPresentation = db.GetVWCRM_PresentationById(model.PresentationId.Value);
+                    string urlMail = TenantConfig.Tenant.GetWebUrl();
+                    var tenantName = TenantConfig.Tenant.TenantName;
+                    string customerText = "";
+                    string channelText = "";
+                    string ownerText = "";
+                    var listMyPersonMails = new List<string>();
+                    var listOtherPersonMails = new List<string>();
+                    if (model.Users != null && model.Users.Count() > 0 && oldPresentation != null)
+                    {
+                        if (oldPresentation.ChannelCompanyId.HasValue)
+                        {
+                            var channelUsers = db.GetVWSH_UserByCompanyIdAndUserIds(oldPresentation.ChannelCompanyId.Value, model.Users.Select(x => x.UserId).ToArray());
+                            channelText = string.Join(", ", channelUsers.Select(t => t.FullName).ToArray());
+                            //if (mailForParticipants)
+                            //{
+                            //    var mail = channelUsers.Select(x => x.email).ToList();
+                            //    if (mail != null) { listOtherPersonMails.AddRange(mail); };
+                            //}
+                        }
+                        if (oldPresentation.CustomerCompanyId.HasValue)
+                        {
+                            var customerUsers = db.GetVWSH_UserByCompanyIdAndUserIds(oldPresentation.CustomerCompanyId.Value, model.Users.Select(x => x.UserId).ToArray());
+                            customerText = string.Join(", ", customerUsers.Select(t => t.FullName).ToArray());
+                            //if (mailForParticipants)
+                            //{
+                            //    var mail = customerUsers.Select(x => x.email).ToList();
+                            //    if (mail != null) { listOtherPersonMails.AddRange(mail); };
+                            //}
+                        }
+                        var owner = db.GetVWSH_UserMyPersonByIds(model.Users.Select(x => x.UserId).ToArray());
+                        ownerText = string.Join(", ", owner.Select(t => t.FullName).ToArray());
+                        listMyPersonMails.AddRange(owner.Select(x => x.email).ToList());
+                        var salesPerson1 = db.GetVWSH_UserById(oldPresentation.SalesPersonId.Value);
+                        if (salesPerson1 != null)
+                        {
+                            if (salesPerson1.Manager1.HasValue)
+                            {
+                                var manager = db.GetVWSH_UserById(salesPerson1.Manager1.Value);
+                                if (manager.email != null) { listMyPersonMails.Add(manager.email); };
+
+                            }
+                        }
+                    }
+                    var myPerson = string.Join(";", listMyPersonMails.ToArray());
+                    var otherPerson = string.Join(";", listOtherPersonMails.ToArray());
+                    string txtKatil = " katılacaktır.";
+                    switch ((EnumCRM_ContactContactStatus)model.ContactStatus)
+                    {
+                        case EnumCRM_ContactContactStatus.ToplantiPlanlandi:
+                            txtKatil = " katılacaktır.";
+                            break;
+                        case EnumCRM_ContactContactStatus.ToplantiGerceklesti:
+                            txtKatil = " katıldı.";
+                            break;
+                        case EnumCRM_ContactContactStatus.ToplantiIptal:
+                            txtKatil = " yer alıyordu.";
+                            break;
+                    }
+
+                    if (!string.IsNullOrEmpty(myPerson))
+                    {
+                        var text = "<h3>Merhaba, </h3>";
+                        text += "<p>{0} firmasına açılan potansiyel randevusu hakkında bilgilendirme; </p>";
+                        text += "<p>{1} {7} ile {2} {8} tarihleri arasında {13} olan {6}</p>";
+                        text += "<p>Firmamızdan {9}</p>";
+                        if (channelText != "")
+                        {
+                            text += "<p>Yanımızda {3}</p>";
+                        }
+                        if (customerText != "")
+                        {
+                            text += "<p>Müşteri tarafında {10}</p>";
+                        }
+                        text += "<p>Randevu hakkında : {11} </p>";
+                        text += "<p>Potansiyelin en son aşaması : {12} </p>";
+                        text += "<p>Görüşme detaylarına gitmek için lütfen <a href='{4}/CRM/VWCRM_Presentation/Detail?id={5}'> Buraya tıklayınız! </a></p>";
+                        text += "<p> Bilgilerinize. </p>";
+
+                        var mesaj = string.Format(text,
+                                oldPresentation.CustomerCompany_Title,
+                                string.Format("{0:dd.MM.yyyy}", model.ContactStartDate),
+                                string.Format("{0:dd.MM.yyyy}", model.ContactEndDate),
+                                channelText != null ? channelText + " " + txtKatil : "",
+                                urlMail,
+                                oldPresentation.id,
+                                Infoline.Helper.EnumsProperties.GetDescriptionFromEnumValue((EnumCRM_ContactContactStatus)model.ContactStatus),
+                                (model.ContactStartDate.Value.Hour < 10 ? "0" + model.ContactStartDate.Value.Hour : model.ContactStartDate.Value.Hour.ToString())
+                                + ":" + (model.ContactStartDate.Value.Minute < 10 ? "0" + model.ContactStartDate.Value.Minute : model.ContactStartDate.Value.Minute.ToString()),
+                                (model.ContactEndDate.Value.Hour < 10 ? "0" + model.ContactEndDate.Value.Hour : model.ContactEndDate.Value.Hour.ToString())
+                                + ":" + (model.ContactEndDate.Value.Minute < 10 ? "0" + model.ContactEndDate.Value.Minute : model.ContactEndDate.Value.Minute.ToString()),
+                                ownerText != null ? ownerText + " " + txtKatil : "",
+                                customerText != null ? customerText + " " + txtKatil : "",
+                                (model.Description != null ? model.Description : " - "),
+                                oldPresentation.Stage_Title,
+                                 model.ContactType == (int)EnumCRM_ContactContactType.Diger ? "randevu tipi belirtilmemiş" : Infoline.Helper.EnumsProperties.GetDescriptionFromEnumValue((EnumCRM_ContactContactType)model.ContactType)
+                                );
+                        new Email().Template("Template1", "potansiyel.jpg", "Yeni Potansiyel Randevusu Hakkında", mesaj)
+                         .Send((Int16)EmailSendTypes.Toplanti, myPerson, string.Format("{0} | {1}", tenantName + " | WORKOFTIME", "Yeni Potansiyel Randevusu Hakkında"), true);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    new Email().Send((Int16)EmailSendTypes.Toplanti, "sahin.elik@infoline-tr.com", "Toplantı Hata", ex.Message);
+                }
                 var rs = model.Save(userId);
                 RenderResponse(context, rs);
             }
