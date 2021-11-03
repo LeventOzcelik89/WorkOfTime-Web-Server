6353115e1220e2651dafcb8841bc4cf8fdbb1f34
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2021-11-03 15:56:04 +0300

    dökümanlar hata giderme

--- Infoline.WorkOfTime/Infoline.WorkOfTime.WebProject/Areas/CMP/Controllers/VWCMP_CompanyFileSelectorController.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.WebProject/Areas/CMP/Controllers/VWCMP_CompanyFileSelectorController.cs
@@ -8,131 +8,88 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-
 using Infoline.WorkOfTime.BusinessAccess.Business;
 using Infoline.Framework.Database;
-
 namespace Infoline.WorkOfTime.WebProject.Areas.CMP.Controllers
 {
-	public class VWCMP_CompanyFileSelectorController : Controller
-	{
-		[AllowEveryone]
-		[PageInfo("Firma İzin Evraklarının Listelendiği Sayfa")]
-		public ActionResult Index()
-		{
-		    return View();
-		}
-		[AllowEveryone]
-		[PageInfo("Firma İzin Evraklarının Listelendiği Metod")]
-		public ContentResult DataSource([DataSourceRequest]DataSourceRequest request)
-		{
-		    var condition = KendoToExpression.Convert(request);
-		    request.Filters = new FilterDescriptor[0];
-		    request.Sorts = new SortDescriptor[0];
-		    request.Page = 1;
-		    var db = new WorkOfTimeDatabase();
-		    var data = db.GetVWCMP_CompanyFileSelector(condition).RemoveGeographies().ToDataSourceResult(request);
-		    data.Total = db.GetVWCMP_CompanyFileSelectorCount(condition.Filter);
-		    return Content(Infoline.Helper.Json.Serialize(data), "application/json");
-		}
-
-		[AllowEveryone]
-		[PageInfo("Firma İzin Evraklarının Listelendiği Metod")]
-		public ActionResult Insert(Guid customerId)
-		{
-		    var data = new VMCMP_CompanyFileSelectorModel { id = Guid.NewGuid(),customerId=customerId };
-		    return View(data);
-		}
-
-		[AllowEveryone]
-		[HttpPost, ValidateAntiForgeryToken]
-		public JsonResult Insert(VMCMP_CompanyFileSelectorModel item)
-		{  
-		    var userStatus = (PageSecurity)Session["userStatus"];
-		    var feedback = new FeedBack();
-			var dbresult = item.Insert(userStatus.user.id);
-			var result = new ResultStatusUI
-		    {
-		        Result = dbresult.result,
-		        FeedBack = dbresult.result ? feedback.Success("Kaydetme işlemi başarılı") : feedback.Error("Kaydetme işlemi başarısız")
-		    };
-		    return Json(result, JsonRequestBehavior.AllowGet);
-		}
-
-		[AllowEveryone]
-		public ActionResult Update(Guid id)
-		{
-		    var db = new WorkOfTimeDatabase();
-		    var data = db.GetVWCMP_CompanyFileSelectorById(id);
-		    return View(data);
-		}
-
-		[AllowEveryone]
-		[HttpPost, ValidateAntiForgeryToken]
-		public JsonResult Update(CMP_CompanyFileSelector item)
-		{
-		    var db = new WorkOfTimeDatabase();
-		    var userStatus = (PageSecurity)Session["userStatus"];
-		    var feedback = new FeedBack();
-		
-		    item.changed = DateTime.Now;
-		    item.changedby = userStatus.user.id;
-		
-		    var dbresult = db.UpdateCMP_CompanyFileSelector(item);
-		    var result = new ResultStatusUI
-		    {
-		        Result = dbresult.result,
-		        FeedBack = dbresult.result ? feedback.Success("Güncelleme işlemi başarılı") : feedback.Error("Güncelleme işlemi başarısız")
-		    };
-		
-		    return Json(result, JsonRequestBehavior.AllowGet);
-		}
-
-
+    public class VWCMP_CompanyFileSelectorController : Controller
+    {
+        [PageInfo("Firma İzin Evraklarının Listelendiği Sayfa", SHRoles.SahaGorevYonetici, SHRoles.SahaGorevOperator, SHRoles.SahaGorevPersonel, SHRoles.SahaGorevMusteri, SHRoles.BayiGorevPersoneli)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AllowEveryone]
+        [PageInfo("Firma İzin Evraklarının Listelendiği Metod", SHRoles.SahaGorevYonetici, SHRoles.SahaGorevOperator, SHRoles.SahaGorevPersonel, SHRoles.SahaGorevMusteri, SHRoles.BayiGorevPersoneli)]
+        public ContentResult DataSource([DataSourceRequest] DataSourceRequest request)
+        {
+            var condition = KendoToExpression.Convert(request);
+            request.Filters = new FilterDescriptor[0];
+            request.Sorts = new SortDescriptor[0];
+            request.Page = 1;
+            var db = new WorkOfTimeDatabase();
+            var data = db.GetVWCMP_CompanyFileSelector(condition).RemoveGeographies().ToDataSourceResult(request);
+            data.Total = db.GetVWCMP_CompanyFileSelectorCount(condition.Filter);
+            return Content(Infoline.Helper.Json.Serialize(data), "application/json");
+        }
+        [AllowEveryone]
+        [PageInfo("Firma İzin Evraklarının Eklendiği Metod", SHRoles.SahaGorevYonetici, SHRoles.SahaGorevOperator, SHRoles.SahaGorevPersonel, SHRoles.SahaGorevMusteri, SHRoles.BayiGorevPersoneli)]
+        public ActionResult Insert(Guid customerId)
+        {
+            var data = new VMCMP_CompanyFileSelectorModel { id = Guid.NewGuid(), customerId = customerId };
+            return View(data);
+        }
+        [PageInfo("Firma İzin Evraklarının Eklendiği   Metod", SHRoles.SahaGorevYonetici, SHRoles.SahaGorevOperator, SHRoles.SahaGorevPersonel, SHRoles.SahaGorevMusteri, SHRoles.BayiGorevPersoneli)]
+        [HttpPost, ValidateAntiForgeryToken]
+        public JsonResult Insert(VMCMP_CompanyFileSelectorModel item)
+        {
+            var userStatus = (PageSecurity)Session["userStatus"];
+            var feedback = new FeedBack();
+            var dbresult = item.Insert(userStatus.user.id);
+            var result = new ResultStatusUI
+            {
+                Result = dbresult.result,
+                FeedBack = dbresult.result ? feedback.Success("Kaydetme işlemi başarılı") : feedback.Error("Kaydetme işlemi başarısız")
+            };
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
         [HttpPost]
-		[AllowEveryone]
+        [PageInfo("Firma İzin Evraklarının Silindiği   Metod", SHRoles.SahaGorevYonetici, SHRoles.SahaGorevOperator, SHRoles.SahaGorevPersonel, SHRoles.SahaGorevMusteri, SHRoles.BayiGorevPersoneli)]
         public JsonResult Delete(Guid id)
         {
             var db = new WorkOfTimeDatabase();
             var feedback = new FeedBack();
-			
-
-
-            var dbresult = new VMCMP_CompanyFileSelectorModel {id=id }.Delete();
-
+            var dbresult = new VMCMP_CompanyFileSelectorModel { id = id }.Delete();
             var result = new ResultStatusUI
             {
                 Result = dbresult.result,
                 FeedBack = dbresult.result ? feedback.Success("Silme işlemi başarılı") : feedback.Warning("Silme işlemi başarısız")
             };
-
             return Json(result, JsonRequestBehavior.AllowGet);
         }
-
-        [AllowEveryone]
-		public JsonResult GetAllDataTableFromSysFile(Guid companyId)
-		{
-			var data = new VMCMP_CompanyFileSelectorModel().GetAllDataTableFromSysFile();
-			return Json(data.objects,JsonRequestBehavior.AllowGet);
-		}
-		[AllowEveryone]
-		public JsonResult GetAllFileGroupNameFromSysFile([DataSourceRequest] DataSourceRequest request, Guid companyId)
-		{
-			var db = new WorkOfTimeDatabase();
-			var existBefore = db.GetVWCMP_CompanyFileSelectorByCustomerId(companyId).ToList();
-			List<SYS_Files> filebase = new List<SYS_Files>();
-			var condition = KendoToExpression.Convert(request);
-			condition.Filter.GetSerializeObject();
-			foreach (var item in LocalFileProvider._dataTableFileGroup.Where(x=>x.Key==condition.Filter.Operand2.ToString()))
-			{
-				var values = item.Value.Where(x => !existBefore.Where(c => c.fileGroupModule == item.Key).Select(a => a.fileGroupName).Contains(x.fileGroup));
-				foreach (var value in values)
-				{
-					filebase.Add(new SYS_Files { FileGroup = value.fileGroup, DataTable = item.Key });
-				}
-			}
-			return Json(filebase, JsonRequestBehavior.AllowGet);
-		}
-
-	}
+        [PageInfo("Ekleme Sayfası için Tüm datatabların çekildiği metod", SHRoles.SahaGorevYonetici, SHRoles.SahaGorevOperator, SHRoles.SahaGorevPersonel, SHRoles.SahaGorevMusteri, SHRoles.BayiGorevPersoneli)]
+        public JsonResult GetAllDataTableFromSysFile(Guid companyId)
+        {
+            var data = new VMCMP_CompanyFileSelectorModel().GetAllDataTableFromSysFile();
+            return Json(data.objects, JsonRequestBehavior.AllowGet);
+        }
+        [PageInfo("Firma İzin Evraklarının Silindiği   Metod", SHRoles.SahaGorevYonetici, SHRoles.SahaGorevOperator, SHRoles.SahaGorevPersonel, SHRoles.SahaGorevMusteri, SHRoles.BayiGorevPersoneli)]
+        public JsonResult GetAllFileGroupNameFromSysFile([DataSourceRequest] DataSourceRequest request, Guid companyId)
+        {
+            var db = new WorkOfTimeDatabase();
+            var existBefore = db.GetVWCMP_CompanyFileSelectorByCustomerId(companyId).ToList();
+            List<SYS_Files> filebase = new List<SYS_Files>();
+            var condition = KendoToExpression.Convert(request);
+            condition.Filter.GetSerializeObject();
+            foreach (var item in LocalFileProvider._dataTableFileGroup.Where(x => x.Key == condition.Filter.Operand2.ToString()))
+            {
+                var values = item.Value.Where(x => !existBefore.Where(c => c.fileGroupModule == item.Key).Select(a => a.fileGroupName).Contains(x.fileGroup));
+                foreach (var value in values)
+                {
+                    filebase.Add(new SYS_Files { FileGroup = value.fileGroup, DataTable = item.Key });
+                }
+            }
+            return Json(filebase, JsonRequestBehavior.AllowGet);
+        }
+    }
 }
