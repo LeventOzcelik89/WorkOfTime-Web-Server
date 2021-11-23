7cc8c5382bfa99f79830b402821235d945897852
Ahmet Ündemir <ahmetundemir@outlook.com>
2021-11-23 13:38:18 +0300

    ProductBounty model,controller hazırlandı.

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/Business/Product/VMPRD_ProductBountyModel.cs
@@ -0,0 +1,126 @@
+﻿using Infoline.Framework.Database;
+using Infoline.Helper;
+using Infoline.WorkOfTime.BusinessData;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Web;
+
+namespace Infoline.WorkOfTime.BusinessAccess
+{
+	public class VMPRD_ProductBountyModel : VWPRD_ProductBounty
+	{
+
+		private WorkOfTimeDatabase db { get; set; }
+		private DbTransaction trans { get; set; }
+
+		public VMPRD_ProductBountyModel Load()
+		{
+
+			db = db ?? new WorkOfTimeDatabase();
+			var productBounty = db.GetVWPRD_ProductBountyById(this.id);
+
+			if (productBounty != null)
+			{
+				this.B_EntityDataCopyForMaterial(productBounty, true);
+			}
+
+			return this;
+		}
+		public ResultStatus Save(Guid? userId, HttpRequestBase request = null, DbTransaction trans = null)
+		{
+
+			db = db ?? new WorkOfTimeDatabase();
+			var productBounty = db.GetVWPRD_ProductBountyById(this.id);
+			var rs = new ResultStatus { result = true };
+
+			if (productBounty == null)
+			{
+				this.createdby = userId;
+				this.created = DateTime.Now;
+				rs = Insert(trans);
+			}
+			else
+			{
+				this.changedby = userId;
+				this.changed = DateTime.Now;
+				rs = Update(trans);
+			}
+
+			return rs;
+		}
+
+		private ResultStatus Insert(DbTransaction trans = null)
+		{
+			db = db ?? new WorkOfTimeDatabase();
+			var transaction = trans ?? db.BeginTransaction();
+			var productBounty = new PRD_ProductBounty().B_EntityDataCopyForMaterial(this, true);
+
+			var rs = db.InsertPRD_ProductBounty(productBounty, transaction);
+
+			if (rs.result == true)
+			{
+				if (trans == null) transaction.Commit();
+				return new ResultStatus { result = true, message = "Ürün Prim Tanımlama İşlemi Başarılı." };
+			}
+			else
+			{
+				if (trans == null) transaction.Rollback();
+				return new ResultStatus { result = false, message = "Ürün Prim Tanımlama İşlemi başarısız." };
+			}
+		}
+		private ResultStatus Update(DbTransaction trans = null)
+		{
+			db = db ?? new WorkOfTimeDatabase();
+			var transaction = trans ?? db.BeginTransaction();
+			var productBounty = new PRD_ProductBounty().B_EntityDataCopyForMaterial(this, true);
+
+			var rs = db.UpdatePRD_ProductBounty(productBounty, true, transaction);
+
+
+			if (rs.result == true)
+			{
+				if (trans == null) transaction.Commit();
+				return new ResultStatus { result = true, message = "Ürün Prim Güncelleme işlemi başarılı." };
+			}
+			else
+			{
+				if (trans == null) transaction.Rollback();
+				return new ResultStatus { result = false, message = "Ürün Prim Güncelleme işlemi başarısız." };
+			}
+		}
+		public ResultStatus Delete(DbTransaction trans = null)
+		{
+			db = db ?? new WorkOfTimeDatabase();
+			var transaction = trans ?? db.BeginTransaction();
+			var _productBounty = db.GetPRD_ProductBountyById(this.id);
+			if (_productBounty == null)
+			{
+				return new ResultStatus { result = false, message = "Ürün prim tanımı silinmiş." };
+			}
+
+			var dbres = new ResultStatus { result = true };
+			dbres &= db.DeletePRD_ProductBounty(_productBounty);
+
+			if (dbres.result == true)
+			{
+				if (trans == null) transaction.Commit();
+				return new ResultStatus
+				{
+					result = true,
+					message = "Ürün Prim silme işlemi başarılı oldu."
+				};
+			}
+			else
+			{
+				if (trans == null) transaction.Rollback();
+				return new ResultStatus
+				{
+					result = false,
+					message = "Ürün Prim silme işlemi başarısız oldu."
+				};
+			}
+		}
+	}
+}
---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/DatabaseFunctions/AutoGenerate/DBVWPRD_ProductBounty.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infoline.Framework.Database;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using System.Data.Common;
+using Infoline.WorkOfTime.BusinessData;
+
+namespace Infoline.WorkOfTime.BusinessAccess
+{
+    partial class WorkOfTimeDatabase
+    {
+        /// <summary>
+        /// VWPRD_ProductBounty tablosundan tüm kayıtları çeken fonksiyondur
+        /// </summary>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>VWPRD_ProductBounty dizi objesini geri döndürür.</returns>
+        public VWPRD_ProductBounty[] GetVWPRD_ProductBounty(DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.Table<VWPRD_ProductBounty>().OrderByDesc(a => a.created).Execute().ToArray();
+            }
+        }
+
+        /// <summary>
+        /// VWPRD_ProductBounty tablosundan simpleQuery objesi filtresi sonucunda gelen kayıtları çeken fonksiyondur. Sayfa giridnde kullanılmaktadır.
+        /// </summary>
+        /// <param name="simpleQuery">Filtre parametreleri olarak obje doldurularak gönderilir.</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+         /// <returns>Filtre Sonucu VWPRD_ProductBounty dizi objesini geri döndürür.</returns>
+        public VWPRD_ProductBounty[] GetVWPRD_ProductBounty(SimpleQuery simpleQuery, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+
+                return db.Table<VWPRD_ProductBounty>().ExecuteSimpleQuery(simpleQuery).ToArray();
+            }
+        }
+        /// <summary>
+        /// VWPRD_ProductBounty tablosundan BEXP Objesi filtresi sonucunda gelen kayıtların toplam adedini veren fonksiyondur.
+        /// </summary>
+        /// <param name="conditionExpression">Filtre parametreleri olarak obje doldurularak gönderilir.</param>
+        /// <returns>Filtre Sonucu Tablo adedini döndürür, sayı(int) olarak.</returns>
+        public int GetVWPRD_ProductBountyCount(BEXP conditionExpression)
+        {
+            using (var db = GetDB())
+            {
+                return db.Table("VWPRD_ProductBounty").Where(conditionExpression).Count();
+            }
+        }
+        /// <summary>
+        /// VWPRD_ProductBounty tablosundan, id si gönderilen kaydın bilgilerini döndüren fonksiyondur.
+        /// </summary>
+        /// <param name="id">VWPRD_ProductBounty tablo id'si verilir.</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>Filtre Sonucu VWPRD_ProductBounty Objesini geri döndürür.</returns>
+        public VWPRD_ProductBounty GetVWPRD_ProductBountyById(Guid id, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+
+            {
+                return db.Table<VWPRD_ProductBounty>().Where(a => a.id == id).Execute().FirstOrDefault();
+            }
+        }
+
+    }
+}
