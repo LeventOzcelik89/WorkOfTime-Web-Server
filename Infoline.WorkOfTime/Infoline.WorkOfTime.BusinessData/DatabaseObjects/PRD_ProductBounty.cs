53689c8aeb6983b0c1e5b8e6da38dfb6b37d8647
Ahmet Ündemir <ahmetundemir@outlook.com>
2021-11-23 13:26:55 +0300

    tt calıstırıldı.

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/PRD_ProductBounty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GeoAPI.Geometries;
+
+namespace Infoline.WorkOfTime.BusinessData
+{
+    public partial class PRD_ProductBounty : InfolineTable
+    {
+        public double? price { get; set;}
+        public Guid? companyId { get; set;}
+        public Guid? personId { get; set;}
+        public Guid? productId { get; set;}
+    }
+}
---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWPRD_ProductBounty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using GeoAPI.Geometries;
+
+namespace Infoline.WorkOfTime.BusinessData
+{
+    public partial class VWPRD_ProductBounty : InfolineTable
+    {
+        public double? price { get; set;}
+        public Guid? companyId { get; set;}
+        public Guid? personId { get; set;}
+        public Guid? productId { get; set;}
+        public string createdby_Title { get; set;}
+        public string changedby_Title { get; set;}
+        public string productionCompanyId_Title { get; set;}
+        public string productId_Title { get; set;}
+        public string personId_Title { get; set;}
+    }
+}
