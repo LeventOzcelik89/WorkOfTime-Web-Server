98302b6c2324bed099c98d8442fd5f6a162d22f9
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2021-11-29 13:08:54 +0300

    TT çalıştırıldı

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/DatabaseFunctions/AutoGenerate/DBPRD_EntegrationImport.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infoline.Framework.Database;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using System.Data.Common;
+using Infoline.WorkOfTime.BusinessData;
+
+namespace Infoline.WorkOfTime.BusinessAccess
+{
+    partial class WorkOfTimeDatabase
+    {
+        /// <summary>
+        /// PRD_EntegrationImport tablosundan tüm kayıtları çeken fonksiyondur
+        /// </summary>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>PRD_EntegrationImport dizi objesini geri döndürür.</returns>
+        public PRD_EntegrationImport[] GetPRD_EntegrationImport(DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.Table<PRD_EntegrationImport>().OrderByDesc(a => a.created).Execute().ToArray();
+            }
+        }
+
+        /// <summary>
+        /// PRD_EntegrationImport tablosundan simpleQuery objesi filtresi sonucunda gelen kayıtları çeken fonksiyondur. Sayfa giridnde kullanılmaktadır.
+        /// </summary>
+        /// <param name="simpleQuery">Filtre parametreleri olarak obje doldurularak gönderilir.</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+         /// <returns>Filtre Sonucu PRD_EntegrationImport dizi objesini geri döndürür.</returns>
+        public PRD_EntegrationImport[] GetPRD_EntegrationImport(SimpleQuery simpleQuery, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+
+                return db.Table<PRD_EntegrationImport>().ExecuteSimpleQuery(simpleQuery).ToArray();
+            }
+        }
+        /// <summary>
+        /// PRD_EntegrationImport tablosundan BEXP Objesi filtresi sonucunda gelen kayıtların toplam adedini veren fonksiyondur.
+        /// </summary>
+        /// <param name="conditionExpression">Filtre parametreleri olarak obje doldurularak gönderilir.</param>
+        /// <returns>Filtre Sonucu Tablo adedini döndürür, sayı(int) olarak.</returns>
+        public int GetPRD_EntegrationImportCount(BEXP conditionExpression)
+        {
+            using (var db = GetDB())
+            {
+                return db.Table("PRD_EntegrationImport").Where(conditionExpression).Count();
+            }
+        }
+        /// <summary>
+        /// PRD_EntegrationImport tablosundan, id si gönderilen kaydın bilgilerini döndüren fonksiyondur.
+        /// </summary>
+        /// <param name="id">PRD_EntegrationImport tablo id'si verilir.</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>Filtre Sonucu PRD_EntegrationImport Objesini geri döndürür.</returns>
+        public PRD_EntegrationImport GetPRD_EntegrationImportById(Guid id, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+
+            {
+                return db.Table<PRD_EntegrationImport>().Where(a => a.id == id).Execute().FirstOrDefault();
+            }
+        }
+
+        /// <summary>
+        /// PRD_EntegrationImport Tablosuna Kayıt Atan Fonksiyondur.
+        /// </summary>
+        /// <param name="item">PRD_EntegrationImport Objesidir. Insert Yapılacak Propertiler Eklenir</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Insert İşlemin Durumunu ve Başarılı Mesajını Geri Döndürür.</returns>
+        public ResultStatus InsertPRD_EntegrationImport(PRD_EntegrationImport item, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteInsert<PRD_EntegrationImport>(item);
+            }
+        }
+
+        /// <summary>
+        /// PRD_EntegrationImport Tablosuna Günceleme Yapan Fonksiyondur.
+        /// </summary>
+        /// <param name="item">PRD_EntegrationImport Objesidir. :Update Yapılacak Propertiler Eklenir</param>
+        /// <param name="setNull">Boş bırakılan propertiler null olarak mı setlensin bilgisi setlenir</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Update İşleminin Durumunu ve Başarılı Mesajını Geri Döndürür.</returns>
+        public ResultStatus UpdatePRD_EntegrationImport(PRD_EntegrationImport item, bool setNull = false, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteUpdate<PRD_EntegrationImport>(item, setNull);
+            }
+        }
+
+        /// <summary>
+        /// PRD_EntegrationImport tablosundan, Verilen kayıt id'si ile Silme İşlemi Yapan Fonksiyondur.
+        /// </summary>
+        /// <param name="id">PRD_EntegrationImport Tablo id'si</param>
+        /// <param name="tran">Mevcut Dışında Farklı Bir Transection Kullanılacak ise Bu Parametreye Gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Silme İşleminin Durumunu ve Başarılı Mesajını Geri Döndürür.</returns>
+        public ResultStatus DeletePRD_EntegrationImport(Guid id, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteDelete<PRD_EntegrationImport>(id);
+            }
+        }
+
+        /// <summary>
+        /// PRD_EntegrationImport tablosundan, Verilen objeler ile Silme İşlemi Yapan Fonksiyondur.
+        /// </summary>
+        /// <param name="item">PRD_EntegrationImport Objesidir. Silme İşlemi Yapılacak Propertiler Eklenir, Eklenenkere Göre Filtrelenerek Silme işlemi yapılır.</param>
+        /// <param name="tran">Mevcut Dışında Farklı Bir Transection Kullanılacak ise Bu Parametreye Gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Silme İşleminin Durumunu ve Başarılı Mesajını Geri Döndürür.</returns>
+        public ResultStatus DeletePRD_EntegrationImport(PRD_EntegrationImport item, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteDelete<PRD_EntegrationImport>(item);
+            }
+        }
+
+        /// <summary>
+        /// PRD_EntegrationImport Tablosuna Toplu insert işlemi yapan fonksiyondur.
+        /// </summary>
+        /// <param name="item">PRD_EntegrationImport Dizisi Gönderilerek insert işlemi yapılacak dizi objesi gönderilir.</param>
+        /// <param name="tran">Mevcut Dışında Farklı Bir Transection Kullanılacak ise Bu Parametreye Gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. insert işlemlerinin sonucunu ve başarılı mesajını geri döndürür.</returns>
+        public ResultStatus BulkInsertPRD_EntegrationImport(IEnumerable<PRD_EntegrationImport> item, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteBulkInsert<PRD_EntegrationImport>(item);
+            }
+        }
+
+        /// <summary>
+        /// PRD_EntegrationImport Tablosuna Toplu Update işlemi yapan fonksiyondur.
+        /// </summary>
+        /// <param name="item">PRD_EntegrationImport Dizisi Gönderilerek Update İşlemi Yapılacak Dizi Objesi Gönderilir.</param>
+        /// <param name="tran">Mevcut Dışında Farklı Bir Transection Kullanılacak ise Bu Parametreye Gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Update İşlemlerinin Sonucunu ve Başarılı Mesajını Geri Döndürür.</returns>
+        public ResultStatus BulkUpdatePRD_EntegrationImport(IEnumerable<PRD_EntegrationImport> item, bool setNull = false, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteBulkUpdate<PRD_EntegrationImport>(item, setNull);
+            }
+        }
+
+        /// <summary>
+        /// PRD_EntegrationImport tablosundan, Verilen Object List ile Toplu Silme İşlemi Yapan Fonksiyondur.
+        /// </summary>
+        /// <param name="item">PRD_EntegrationImport Dizisi Gönderilerek Delete işlemi yapılacak dizi objesi göderilir.</param>
+        /// <param name="tran">Mevcut Dışında Farklı Bir Transection Kullanılacak ise Bu Parametreye Gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Delete işlemlerinin sonucunu ve başarılı mesajını geri döndürür.</returns>
+        public ResultStatus BulkDeletePRD_EntegrationImport(IEnumerable<PRD_EntegrationImport> item, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteBulkDelete<PRD_EntegrationImport>(item);
+            }
+        }
+
+    }
+}
---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWPRD_EntegrationImport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using GeoAPI.Geometries;
+
+namespace Infoline.WorkOfTime.BusinessData
+{
+    public partial class VWPRD_EntegrationImport : InfolineTable
+    {
+        public string customerName { get; set;}
+        public string customerCode { get; set;}
+        public string productModel { get; set;}
+        public string distributorName { get; set;}
+        public DateTime? distributorConfirmationDate { get; set;}
+        public string imei { get; set;}
+        public string customerType { get; set;}
+        public DateTime? contractStartDate { get; set;}
+        public DateTime? contractEndDate { get; set;}
+        public string contractCode { get; set;}
+        public string productGroup { get; set;}
+        public string sellingChannelType { get; set;}
+        public string distributorCode { get; set;}
+        public int? sellingQuantity { get; set;}
+        public int? month { get; set;}
+        public int? year { get; set;}
+        public string createdby_Title { get; set;}
+        public string changedby_Title { get; set;}
+        public Guid? companyId_Title { get; set;}
+        public Guid? inventoryId_Title { get; set;}
+        public Guid? distributor_Title { get; set;}
+    }
+}
