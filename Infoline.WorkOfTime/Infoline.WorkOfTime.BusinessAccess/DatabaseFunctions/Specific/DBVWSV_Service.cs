587fe850c927a200a33d00e1b18d482729dee432
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2021-12-20 10:07:55 +0300

    enumlar oluşturuldu

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/DatabaseFunctions/Specific/DBVWSV_Service.cs
@@ -0,0 +1,20 @@
+﻿using Infoline.WorkOfTime.BusinessData;
+using System.ComponentModel;
+namespace Infoline.WorkOfTime.BusinessAccess
+{
+    [EnumInfo(typeof(SV_Service), "DeliveryType")]
+    public enum EnumSV_Service
+    {
+        [Description("Bayi")]
+        CompanyPerson = 0,
+        [Description("Elden Teslim")]
+        HandedOver = 1,
+        [Description("Kargo")]
+        Cargo = 2,
+        [Description("Diğer")]
+        Other = 3,
+    }
+    partial class WorkOfTimeDatabase
+    {
+    }
+}
