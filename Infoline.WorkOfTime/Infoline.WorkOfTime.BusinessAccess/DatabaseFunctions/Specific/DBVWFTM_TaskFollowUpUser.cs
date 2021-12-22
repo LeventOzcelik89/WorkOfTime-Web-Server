a9e8fd5b8091994dcff28ad72456ad0060493d53
furkanyilmaz-infoline <furkan.yilmaz@infoline-tr.com>
2021-12-22 13:50:11 +0300

    saha görevleri detay modeline takip edecek personeller eklendi, webservice

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/DatabaseFunctions/Specific/DBVWFTM_TaskFollowUpUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infoline.Framework.Database;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using System.Data.Common;
+using Infoline.WorkOfTime.BusinessData;
+
+namespace Infoline.WorkOfTime.BusinessAccess
+{
+    partial class WorkOfTimeDatabase
+    {
+        public VWFTM_TaskFollowUpUser[] GetVWFTM_TaskFollowUpUserByTaskId(Guid taskId, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.Table<VWFTM_TaskFollowUpUser>().Where(a => a.taskId == taskId).Execute().ToArray();
+            }
+        }
+    }
+}
