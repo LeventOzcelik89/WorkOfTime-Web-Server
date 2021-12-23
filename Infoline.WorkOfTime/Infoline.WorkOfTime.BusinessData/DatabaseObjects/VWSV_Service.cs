9ea4b03ac78f556795f58ffa268e8d6d50c3445a
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2021-12-23 14:29:13 +0300

    database fonksiyonları oluşturuldu

--- Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWSV_Service.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWSV_Service.cs
@@ -26,6 +26,7 @@ namespace Infoline.WorkOfTime.BusinessData
         public string delivery_Title { get; set;}
         public string customerType_Title { get; set;}
         public string serialCode { get; set;}
+        public string product_Title { get; set;}
         public short? lastOperationStatus { get; set;}
     }
 }
