fb745a24c5f512d211f8ad53d0c4d06774477b16
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2022-01-07 13:02:44 +0300

    tt çalıştırıldı

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWINV_PermitSummary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using GeoAPI.Geometries;
+
+namespace Infoline.WorkOfTime.BusinessData
+{
+    public partial class VWINV_PermitSummary : InfolineTable
+    {
+        public int PermitYearlyDeserved { get; set;}
+        public int PermitYearlyUsed { get; set;}
+        public double PermitExcuseDeserved { get; set;}
+        public double PermitExcuseUsed { get; set;}
+        public string Name { get; set;}
+        public bool? IsPaidPermit { get; set;}
+        public int? PaidPermitDay { get; set;}
+        public bool? CanHourly { get; set;}
+        public string Descriptions { get; set;}
+        public bool? BeDocumented { get; set;}
+        public bool? RequestStaff { get; set;}
+        public bool? ViewStaff { get; set;}
+        public string IsPaidPermit_Title { get; set;}
+        public string CanHourly_Title { get; set;}
+        public string BeDocumented_Title { get; set;}
+        public Guid? userId { get; set;}
+        public string userId_Title { get; set;}
+        public Guid? companyId { get; set;}
+        public string companyId_Title { get; set;}
+        public Guid? DepartmentId { get; set;}
+        public string departmentId_Title { get; set;}
+        public string permitTypeId { get; set;}
+        public DateTime? employeeJobStartDate { get; set;}
+        public DateTime? employeeDateOfBirth { get; set;}
+        public bool? employeeStatus { get; set;}
+        public string Type_Title { get; set;}
+        public string Status_Title { get; set;}
+    }
+}
