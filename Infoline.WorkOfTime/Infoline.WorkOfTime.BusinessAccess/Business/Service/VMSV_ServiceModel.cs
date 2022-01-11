f7c74fa18a57a5791a395e17100e32339fd45db8
Seyit Hamit TEKCE <seyit.tekce@infoline-tr.com>
2022-01-11 11:12:08 +0300

    service model id güncellemesi

--- Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/Business/Service/VMSV_ServiceModel.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/Business/Service/VMSV_ServiceModel.cs
@@ -391,7 +391,7 @@ namespace Infoline.WorkOfTime.BusinessAccess
                  text += "<p>" + imei.serialcode + " kodlu cihaz teknik servisimize gelmiştir</p>";
 
                 text += "<p>Bilgilerinize.</p>";
-                new Email().Template("Template1", "bos.png", TenantConfig.Tenant.TenantName + " | Teknik Servis Bildirimi Hakkında", text).Send((Int16)EmailSendTypes.ZorunluMailler, data.Customer.email, $"{TenantConfig.Tenant.TenantName } | Teknik Servis Bildirimi", true,null,null,new string[] { $"{TenantConfig.Tenant.GetWebUrl()}/SV/VWSV_Service/Print?serviceId={data.id}"},false);
+                new Email().Template("Template1", "bos.png", TenantConfig.Tenant.TenantName + " | Teknik Servis Bildirimi Hakkında", text).Send((Int16)EmailSendTypes.ZorunluMailler, data.Customer.email, $"{TenantConfig.Tenant.TenantName } | Teknik Servis Bildirimi", true,null,null,new string[] { $"{TenantConfig.Tenant.GetWebUrl()}/SV/VWSV_Service/Print?id={data.id}"},false);
             }
 
 
