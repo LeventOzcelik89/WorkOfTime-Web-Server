7c5eb93503face9c1d5e2616db4e3c81ade1e93e
Ahmet Ündemir <ahmetundemir@outlook.com>
2022-01-13 13:43:53 +0300

    DÜZENLEME

--- Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/Business/CMP/VMCMP_RequestModels.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/Business/CMP/VMCMP_RequestModels.cs
@@ -45,14 +45,12 @@ namespace Infoline.WorkOfTime.BusinessAccess
 
 		public VMCMP_RequestModels Load(bool? isTransform)
 		{
-			//Kontrol Edilecek.
 			db = db ?? new WorkOfTimeDatabase();
 			var invoice = db.GetCMP_InvoiceById(this.id);
-			var request = db.GetVWCMP_InvoiceById(this.id);
 
 			if (invoice != null)
 			{
-				this.B_EntityDataCopyForMaterial(request, true);
+				this.B_EntityDataCopyForMaterial(invoice, true);
 				this.InvoiceItems = db.GetVWCMP_InvoiceItemByInvoiceId(this.id).OrderBy(a => a.itemOrder).ToList();
 				this.InvoiceActions = db.GetVWCMP_InvoiceActionByInvoiceId(this.id).ToList();
 
@@ -70,12 +68,12 @@ namespace Infoline.WorkOfTime.BusinessAccess
 						}
 					}
 
-					if (request != null)
+					if (invoice != null)
 					{
-						var tender = db.GetVWCMP_InvoiceByPid(request.id);
+						var tender = db.GetVWCMP_InvoiceByPid(invoice.id);
 						if (tender != null)
 						{
-							Tender = db.GetVWCMP_InvoiceByPid(tender.id);
+							Tender = db.GetVWCMP_InvoiceByPid(invoice.id);
 						}
 					}
 
