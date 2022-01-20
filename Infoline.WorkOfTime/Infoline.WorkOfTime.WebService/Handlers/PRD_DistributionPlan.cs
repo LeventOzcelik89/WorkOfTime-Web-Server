ce1e563cea06763aadcab1e57a2d4ec6c2bdc825
Ahmet Ündemir <ahmetundemir@outlook.com>
2022-01-20 14:00:38 +0300

    PRD_ProductUnit servisi eklendi.

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.WebService/Handlers/PRD_DistributionPlan.cs
@@ -0,0 +1,162 @@
+﻿using Infoline.Framework.Database;
+using Infoline.Web.SmartHandlers;
+using Infoline.WorkOfTime.BusinessAccess;using Infoline.WorkOfTime.BusinessData;using System;
+using System.ComponentModel.Composition;
+using System.Web;
+
+namespace Infoline.WorkOfTime.WebService
+{
+    [Export(typeof(ISmartHandler))]
+    public partial class PRD_DistributionPlanHandler : BaseSmartHandler
+    {
+        public PRD_DistributionPlanHandler()
+            : base("PRD_DistributionPlan")
+        {
+
+        }
+
+        /// <summary>
+        /// PRD_DistributionPlan tablosundaki kayıtları listelemek için kullanılan Web Servisdir..
+        /// </summary>
+        /// <param name="context">Opsiyonel olarak [Condition] nesnesi alır. Parametreleri ise;
+        /// Filter = [ Field, Operator, Value ] dizi şeklinde alır.
+        ///     Field =         Tablodaki kolon adı
+        ///     Operator =
+        ///         Equal               Eşittir
+        ///         NotEqual            Eşit Değildir
+        ///         GreaterThan         Büyüktür
+        ///         GreaterThanOrEqual  Büyüktür veya Eşittir
+        ///         LessThan            Küçüktür
+        ///         LessThanOrEqual     Küçüktür veya Eşittir
+        ///         In                  İçerir, Dizi formatında
+        ///     Sort =          Field ve Type olarak 2 property alır
+        ///         Field = Tablodaki kolon adı
+        ///         Type = "ASC" ve "DESC" değerlerini alır
+        ///     Fields =        Tablodan çekilecek alanlar belirtilir. Dizi şeklinde string olarak giriş yapılır. Örn : ["id","Name"]. Yazılmaz ise tüm kolonlar gelir.
+        ///     StartIndex =    Kayıtların alınmaya başlanacağı sırayı belirtir. Örn 100 değeri girilir ise ilk 100 kayıt alınmaz. Kayıtlar 101. kayıttan itibaren gelmeye başlar.
+        ///     Count =         Kayıtların toplamda en fazla kaç tane geleceğinin belirtildiği propertydir. Örn : 200 Denir ise en fazla 200 satır kayıt gelir.
+        /// </param>
+        [HandleFunction("PRD_DistributionPlan/GetAll")]
+        public void PRD_DistributionPlanGetAll(HttpContext context)
+        {
+            try
+            {
+                var c = ParseRequest<Condition>(context);
+                var cond = c != null ? CondtionToQuery.Convert(c) : new SimpleQuery();
+                var db = new WorkOfTimeDatabase();
+                var data = db.GetPRD_DistributionPlan(cond);
+                RenderResponse(context, data);
+            }
+            catch (Exception ex)
+            {
+                RenderResponse(context, new ResultStatus() { result = false, message = ex.Message.ToString() });
+            }
+        }
+
+        /// <summary>
+        /// PRD_DistributionPlan tablosundan id ye göre tekil kayıt çekmek için kullanılan Web Servisdir..
+        /// </summary>
+        /// <param name="context">
+        /// [id] parametresini alır. Guid.
+        /// </param>
+        [HandleFunction("PRD_DistributionPlan/GetById")]
+        public void PRD_DistributionPlanGetById(HttpContext context)
+        {
+            try
+            {
+                var db = new WorkOfTimeDatabase();
+                var id = context.Request["id"];
+                var data = db.GetPRD_DistributionPlanById(new Guid((string)id));
+                RenderResponse(context, data);
+            }
+            catch (Exception ex)
+            {
+                RenderResponse(context, new ResultStatus() { result = false, message = ex.Message.ToString() });
+            }
+        }
+
+        /// <summary>
+        /// PRD_DistributionPlan Tablosuna kayıt ekleme işlemi için kullanılan Web Servisdir..
+        /// </summary>
+        /// <param name="context">
+        /// Parametre olarak PRD_DistributionPlan nesnesinden alır. ( JSON formatında )
+        /// </param>
+        [HandleFunction("PRD_DistributionPlan/Insert")]
+        public void PRD_DistributionPlanInsert(HttpContext context)
+        {
+            try
+            {
+            var db = new WorkOfTimeDatabase();
+            var data = ParseRequest<PRD_DistributionPlan>(context);
+            var dbresult = db.InsertPRD_DistributionPlan(data);
+
+
+            RenderResponse(context, dbresult);
+            }
+            catch (Exception ex)
+            {
+                RenderResponse(context, new ResultStatus() { result = false, message = ex.Message.ToString() });
+            }
+        }
+
+        /// <summary>
+        /// PRD_DistributionPlan Tablosunda kayıt güncellemek için kullanılan Web Servisdir.
+        /// </summary>
+        /// <param name="context">
+        /// Parametre olarak PRD_DistributionPlan nesnesinden alır. ( JSON formatında )
+        /// </param>
+        [HandleFunction("PRD_DistributionPlan/Update")]
+        public void PRD_DistributionPlanUpdate(HttpContext context)
+        {
+            try
+            {
+
+                var db = new WorkOfTimeDatabase();
+                var data = ParseRequest<PRD_DistributionPlan>(context);
+                var dbresult = db.UpdatePRD_DistributionPlan(data);
+
+                RenderResponse(context, dbresult);
+            }
+            catch (Exception ex)
+            {
+                RenderResponse(context, new ResultStatus() { result = false, message = ex.Message.ToString() });
+            }
+        }
+
+        /// <summary>
+        /// PRD_DistributionPlan Tablosundan kayıt silmek için kullanılan servistir.
+        /// </summary>
+        /// <param name="context">
+        /// tekil parametre olarak id ( Guid ) alanıda gönderilebilir.
+        /// PRD_DistributionPlan Nesnesi de gönderilebilir.
+        /// </param>
+        [HandleFunction("PRD_DistributionPlan/Delete")]
+        public void PRD_DistributionPlanDelete(HttpContext context)
+        {
+            try
+            {
+                var dbresult = new ResultStatus();
+                var db = new WorkOfTimeDatabase();
+                var id = context.Request["id"];
+                if (id != null)
+                {
+                    dbresult = db.DeletePRD_DistributionPlan(new Guid((string)id));
+                }
+                else
+                {
+                    var item = ParseRequest<PRD_DistributionPlan>(context);
+                    dbresult = db.DeletePRD_DistributionPlan(item);
+                }
+
+
+                RenderResponse(context, dbresult);
+
+            }
+            catch (Exception ex)
+            {
+                RenderResponse(context, new ResultStatus() { result = false, message = ex.Message.ToString() });
+            }
+        }
+
+    }
+}
