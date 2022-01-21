af4f9a9f7a5feb367061cb1af61fea3bd9c39ff3
furkaneroglu-infoline <furkan.eroglu@infoline-tr.com>
2022-01-21 11:37:55 +0300

    MappAll Sayfa revizesi

--- Infoline.WorkOfTime/Infoline.WorkOfTime.WebProject/Areas/SH/Controllers/SH_UserLocationTrackingController.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.WebProject/Areas/SH/Controllers/SH_UserLocationTrackingController.cs
@@ -4,14 +4,11 @@ using MessagingToolkit.QRCode.Codec;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Processing;
 
 namespace Infoline.WorkOfTime.WebProject.Areas.SH.Controllers
 {
@@ -62,6 +59,7 @@ namespace Infoline.WorkOfTime.WebProject.Areas.SH.Controllers
         }
 
 
+
         [AllowEveryone]
         public ContentResult RedMark(Guid id)//// bool ccc ekleyip arka tarafa green resmini getir.
         {
@@ -69,6 +67,7 @@ namespace Infoline.WorkOfTime.WebProject.Areas.SH.Controllers
 
             var db = new WorkOfTimeDatabase();
             var profil = db.GetSysFilesFilePathByDataTableAndFileGroupAndDataId("SH_User", "Profil Resmi", id);// dbden pp fotoları geliyor
+
             if (profil == null || !System.IO.File.Exists(Server.MapPath(profil.FilePath)))// Profil reesmi olmayanlara boş profil resmi ataması yapılıyor.
             {
 
@@ -91,183 +90,136 @@ namespace Infoline.WorkOfTime.WebProject.Areas.SH.Controllers
 
 
 
-            //        var mark = new SixLabors.ImageSharp.Image.Load(Server.MapPath("/Content/Custom/img/PersonsBackImage/mark.png"));// markın bulunduğu alan
-            //        var profilFoto = new System.Drawing.Bitmap(Server.MapPath(profil.FilePath));
+            var mark = "~/Content/Custom/img/PersonsBackImage/mark.png";// markın bulunduğu alan
+            var profilFoto = "~" + profil.FilePath;
 
+            var merged = ImageProcessing.MergePictures(mark, profilFoto);
 
-
-            using (SixLabors.ImageSharp.Image prof = SixLabors.ImageSharp.Image.Load(Server.MapPath(profil.FilePath)))
-            {
-                using (SixLabors.ImageSharp.Image markk = SixLabors.ImageSharp.Image.Load(Server.MapPath("/Content/Custom/img/PersonsBackImage/mark.png")))
-                {
-
-                    prof.Mutate(a => a.Resize(40, 40));
-                    markk.Mutate(a => a.DrawImage(prof, new SixLabors.ImageSharp.Point(12, 1), 1f));
-                    prof.Save("c:/tt/img1.jpg");
-                    markk.Save("c:/tt/img2.jpg");
-                }
-            }
-
-
-            //var graph = Graphics.FromImage(profilFoto);
-            //using (Brush br = new SolidBrush(Color.Transparent))
-            //{
-            //    graph.CompositingMode = CompositingMode.SourceCopy;
-            //    graph.CompositingQuality = CompositingQuality.HighQuality;
-            //    graph.InterpolationMode = InterpolationMode.HighQualityBicubic;
-            //    graph.SmoothingMode = SmoothingMode.HighQuality;
-            //    graph.PixelOffsetMode = PixelOffsetMode.HighQuality;
-            //    graph.FillRectangle(br, 0, 0, 40, 40);
-            //}
-            //GraphicsPath path = new GraphicsPath();
-            //path.AddEllipse(new Rectangle(0, 0, 40, 40));
-            //graph.SetClip(path);
-            //graph.DrawImage(profilFoto, 0, 0);
-
-            //// Markın üztüne profil fotoğrafının basıldığı yer
-            //using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(mark))
-            //{
-            //    g.CompositingQuality = CompositingQuality.HighQuality;
-            //    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
-            //    g.SmoothingMode = SmoothingMode.HighQuality;
-            //    var bm = new Bitmap(profilFoto.Width, profilFoto.Height, graph);
-            //    bm.Save("c:/tt/bm.png");
-            //    g.DrawImage(bm, new System.Drawing.Rectangle(12, 1, 40, 40));
-            //}
-
-            //using (var ms = new MemoryStream())
-            //{
-
-            //    mark.Save(ms, ImageFormat.Png);// format değişince background siyah yerine oluşan imleç çıkıyor sadece
-            //    mark.Save("c:/tt/mm.png");
-            //    Response.Clear();
-            //    Response.ContentType = "image/png";
-            //    Response.AddHeader("content-disposition", "attachment; qr.png");
-            //    Response.BinaryWrite(ms.ToArray());
-            //    Response.End();
-
-            //}
+            Response.Clear();
+            Response.ContentType = "image/png";
+            Response.AddHeader("content-disposition", "attachment; qr.png");
+            Response.BinaryWrite(merged);
+            Response.End();
 
             return Content(null);
 
+
+
+
+            //        [AllowEveryone]
+            //        public ContentResult GreenMark(Guid id)
+            //        {
+
+            //            var db = new WorkOfTimeDatabase();
+            //            var profil = db.GetSysFilesFilePathByDataTableAndFileGroupAndDataId("SH_User", "Profil Resmi", id);// dbden pp fotoları geliyor
+
+
+            //            var GreenMark = new System.Drawing.Bitmap(Server.MapPath("/Content/Custom/img/PersonsBackImage/GreenMark.png"));// markın bulunduğu alan
+
+            //#if DEBUG
+            //            var profilFoto = new System.Drawing.Bitmap(Server.MapPath(profil.FilePath));
+
+            //            //  kalite bozulmadan resize
+            //            //  var profil2 = new Bitmap(profilFoto, 40, 40);
+
+
+            //            //Profil fotoğrafının circle yapıldığı yer
+            //            Image dstImage = new Bitmap(profilFoto.Width, profilFoto.Height, profilFoto.PixelFormat);
+            //            Graphics c = Graphics.FromImage(dstImage);
+            //            using (Brush br = new SolidBrush(Color.White))
+            //            {
+            //                c.CompositingMode = CompositingMode.SourceCopy;
+            //                c.CompositingQuality = CompositingQuality.HighQuality;
+            //                c.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //                c.SmoothingMode = SmoothingMode.HighQuality;
+            //                c.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            //                c.FillRectangle(br, 0, 0, 40, 40);
+            //            }
+            //            GraphicsPath path = new GraphicsPath();
+            //            path.AddEllipse(new Rectangle(0, 0, dstImage.Width, dstImage.Height));
+            //            c.SetClip(path);
+            //            c.DrawImage(profilFoto, 0, 0);
+
+            //            // Markın üztüne profil fotoğrafının basıldığı yer
+            //            using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(GreenMark))
+            //            {
+            //                g.CompositingQuality = CompositingQuality.HighQuality;
+            //                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //                g.SmoothingMode = SmoothingMode.HighQuality;
+            //                g.DrawImage(dstImage, new System.Drawing.Rectangle(12, 1, 40, 40));
+
+            //            }
+
+
+
+
+            //            using (var ms = new MemoryStream())
+            //            {
+
+            //                GreenMark.Save(ms, ImageFormat.Png);// format değişince background siyah yerine oluşan imleç çıkıyor sadece
+
+            //                Response.Clear();
+            //                Response.ContentType = "image/png";
+            //                Response.AddHeader("content-disposition", "attachment; qr.png");
+            //                Response.BinaryWrite(ms.ToArray());
+            //                Response.End();
+
+            //            }
+
+            //            return Content(null);
+            //#endif
+            //            var profilFotos = new System.Drawing.Bitmap(Server.MapPath(profil.FilePath));
+
+            //            //  kalite bozulmadan resize
+            //            //  var profil2 = new Bitmap(profilFoto, 40, 40);
+
+
+            //            //Profil fotoğrafının circle yapıldığı yer
+            //            Image dstImages = new Bitmap(profilFotos.Width, profilFotos.Height, profilFotos.PixelFormat);
+            //            Graphics cs = Graphics.FromImage(dstImages);
+            //            using (Brush br = new SolidBrush(Color.White))
+            //            {
+            //                cs.CompositingMode = CompositingMode.SourceCopy;
+            //                cs.CompositingQuality = CompositingQuality.HighQuality;
+            //                cs.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //                cs.SmoothingMode = SmoothingMode.HighQuality;
+            //                cs.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            //                cs.FillRectangle(br, 0, 0, 40, 40);
+            //            }
+            //            GraphicsPath paths = new GraphicsPath();
+            //            paths.AddEllipse(new Rectangle(0, 0, dstImage.Width, dstImage.Height));
+            //            c.SetClip(paths);
+            //            c.DrawImage(profilFotos, 0, 0);
+
+            //            // Markın üztüne profil fotoğrafının basıldığı yer
+            //            using (System.Drawing.Graphics gs = System.Drawing.Graphics.FromImage(GreenMark))
+            //            {
+            //                gs.CompositingQuality = CompositingQuality.HighQuality;
+            //                gs.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            //                gs.SmoothingMode = SmoothingMode.HighQuality;
+            //                gs.DrawImage(dstImage, new System.Drawing.Rectangle(12, 1, 40, 40));
+
+            //            }
+
+
+
+
+            //            using (var ms = new MemoryStream())
+            //            {
+
+            //                GreenMark.Save(ms, ImageFormat.Png);// format değişince background siyah yerine oluşan imleç çıkıyor sadece
+
+            //                Response.Clear();
+            //                Response.ContentType = "image/png";
+            //                Response.AddHeader("content-disposition", "attachment; qr.png");
+            //                Response.BinaryWrite(ms.ToArray());
+            //                Response.End();
+
+            //            }
+
+            //            return Content(null);
+            //        }
+
         }
-
-
-//        [AllowEveryone]
-//        public ContentResult GreenMark(Guid id)
-//        {
-
-//            var db = new WorkOfTimeDatabase();
-//            var profil = db.GetSysFilesFilePathByDataTableAndFileGroupAndDataId("SH_User", "Profil Resmi", id);// dbden pp fotoları geliyor
-
-
-//            var GreenMark = new System.Drawing.Bitmap(Server.MapPath("/Content/Custom/img/PersonsBackImage/GreenMark.png"));// markın bulunduğu alan
-
-//#if DEBUG
-//            var profilFoto = new System.Drawing.Bitmap(Server.MapPath(profil.FilePath));
-
-//            //  kalite bozulmadan resize
-//            //  var profil2 = new Bitmap(profilFoto, 40, 40);
-
-
-//            //Profil fotoğrafının circle yapıldığı yer
-//            Image dstImage = new Bitmap(profilFoto.Width, profilFoto.Height, profilFoto.PixelFormat);
-//            Graphics c = Graphics.FromImage(dstImage);
-//            using (Brush br = new SolidBrush(Color.White))
-//            {
-//                c.CompositingMode = CompositingMode.SourceCopy;
-//                c.CompositingQuality = CompositingQuality.HighQuality;
-//                c.InterpolationMode = InterpolationMode.HighQualityBicubic;
-//                c.SmoothingMode = SmoothingMode.HighQuality;
-//                c.PixelOffsetMode = PixelOffsetMode.HighQuality;
-//                c.FillRectangle(br, 0, 0, 40, 40);
-//            }
-//            GraphicsPath path = new GraphicsPath();
-//            path.AddEllipse(new Rectangle(0, 0, dstImage.Width, dstImage.Height));
-//            c.SetClip(path);
-//            c.DrawImage(profilFoto, 0, 0);
-
-//            // Markın üztüne profil fotoğrafının basıldığı yer
-//            using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(GreenMark))
-//            {
-//                g.CompositingQuality = CompositingQuality.HighQuality;
-//                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
-//                g.SmoothingMode = SmoothingMode.HighQuality;
-//                g.DrawImage(dstImage, new System.Drawing.Rectangle(12, 1, 40, 40));
-
-//            }
-
-
-
-
-//            using (var ms = new MemoryStream())
-//            {
-
-//                GreenMark.Save(ms, ImageFormat.Png);// format değişince background siyah yerine oluşan imleç çıkıyor sadece
-
-//                Response.Clear();
-//                Response.ContentType = "image/png";
-//                Response.AddHeader("content-disposition", "attachment; qr.png");
-//                Response.BinaryWrite(ms.ToArray());
-//                Response.End();
-
-//            }
-
-//            return Content(null);
-//#endif
-//            var profilFotos = new System.Drawing.Bitmap(Server.MapPath(profil.FilePath));
-
-//            //  kalite bozulmadan resize
-//            //  var profil2 = new Bitmap(profilFoto, 40, 40);
-
-
-//            //Profil fotoğrafının circle yapıldığı yer
-//            Image dstImages = new Bitmap(profilFotos.Width, profilFotos.Height, profilFotos.PixelFormat);
-//            Graphics cs = Graphics.FromImage(dstImages);
-//            using (Brush br = new SolidBrush(Color.White))
-//            {
-//                cs.CompositingMode = CompositingMode.SourceCopy;
-//                cs.CompositingQuality = CompositingQuality.HighQuality;
-//                cs.InterpolationMode = InterpolationMode.HighQualityBicubic;
-//                cs.SmoothingMode = SmoothingMode.HighQuality;
-//                cs.PixelOffsetMode = PixelOffsetMode.HighQuality;
-//                cs.FillRectangle(br, 0, 0, 40, 40);
-//            }
-//            GraphicsPath paths = new GraphicsPath();
-//            paths.AddEllipse(new Rectangle(0, 0, dstImage.Width, dstImage.Height));
-//            c.SetClip(paths);
-//            c.DrawImage(profilFotos, 0, 0);
-
-//            // Markın üztüne profil fotoğrafının basıldığı yer
-//            using (System.Drawing.Graphics gs = System.Drawing.Graphics.FromImage(GreenMark))
-//            {
-//                gs.CompositingQuality = CompositingQuality.HighQuality;
-//                gs.InterpolationMode = InterpolationMode.HighQualityBicubic;
-//                gs.SmoothingMode = SmoothingMode.HighQuality;
-//                gs.DrawImage(dstImage, new System.Drawing.Rectangle(12, 1, 40, 40));
-
-//            }
-
-
-
-
-//            using (var ms = new MemoryStream())
-//            {
-
-//                GreenMark.Save(ms, ImageFormat.Png);// format değişince background siyah yerine oluşan imleç çıkıyor sadece
-
-//                Response.Clear();
-//                Response.ContentType = "image/png";
-//                Response.AddHeader("content-disposition", "attachment; qr.png");
-//                Response.BinaryWrite(ms.ToArray());
-//                Response.End();
-
-//            }
-
-//            return Content(null);
-//        }
-
-
-
     }
 }
