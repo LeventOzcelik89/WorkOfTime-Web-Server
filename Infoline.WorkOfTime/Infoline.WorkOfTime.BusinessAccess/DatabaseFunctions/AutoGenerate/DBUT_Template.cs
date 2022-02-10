bc914275489d0ad2b207cc4fda34a10973520d3c
Okan Uregen <okan.uregen@infoline-tr.com>
2022-02-10 14:44:36 +0300

    UT_Template oluşturuldu

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/DatabaseFunctions/AutoGenerate/DBUT_Template.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infoline.Framework.Database;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using System.Data.Common;
+using Infoline.WorkOfTime.BusinessData;
+
+namespace Infoline.WorkOfTime.BusinessAccess
+{
+    partial class WorkOfTimeDatabase
+    {
+        /// <summary>
+        /// UT_Template tablosundan tüm kayıtları çeken fonksiyondur
+        /// </summary>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>UT_Template dizi objesini geri döndürür.</returns>
+        public UT_Template[] GetUT_Template(DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.Table<UT_Template>().OrderByDesc(a => a.created).Execute().ToArray();
+            }
+        }
+
+        /// <summary>
+        /// UT_Template tablosundan simpleQuery objesi filtresi sonucunda gelen kayıtları çeken fonksiyondur. Sayfa giridnde kullanılmaktadır.
+        /// </summary>
+        /// <param name="simpleQuery">Filtre parametreleri olarak obje doldurularak gönderilir.</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+         /// <returns>Filtre Sonucu UT_Template dizi objesini geri döndürür.</returns>
+        public UT_Template[] GetUT_Template(SimpleQuery simpleQuery, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+
+                return db.Table<UT_Template>().ExecuteSimpleQuery(simpleQuery).ToArray();
+            }
+        }
+        /// <summary>
+        /// UT_Template tablosundan BEXP Objesi filtresi sonucunda gelen kayıtların toplam adedini veren fonksiyondur.
+        /// </summary>
+        /// <param name="conditionExpression">Filtre parametreleri olarak obje doldurularak gönderilir.</param>
+        /// <returns>Filtre Sonucu Tablo adedini döndürür, sayı(int) olarak.</returns>
+        public int GetUT_TemplateCount(BEXP conditionExpression)
+        {
+            using (var db = GetDB())
+            {
+                return db.Table("UT_Template").Where(conditionExpression).Count();
+            }
+        }
+        /// <summary>
+        /// UT_Template tablosundan, id si gönderilen kaydın bilgilerini döndüren fonksiyondur.
+        /// </summary>
+        /// <param name="id">UT_Template tablo id'si verilir.</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>Filtre Sonucu UT_Template Objesini geri döndürür.</returns>
+        public UT_Template GetUT_TemplateById(Guid id, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+
+            {
+                return db.Table<UT_Template>().Where(a => a.id == id).Execute().FirstOrDefault();
+            }
+        }
+
+        /// <summary>
+        /// UT_Template Tablosuna Kayıt Atan Fonksiyondur.
+        /// </summary>
+        /// <param name="item">UT_Template Objesidir. Insert Yapılacak Propertiler Eklenir</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Insert İşlemin Durumunu ve Başarılı Mesajını Geri Döndürür.</returns>
+        public ResultStatus InsertUT_Template(UT_Template item, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteInsert<UT_Template>(item);
+            }
+        }
+
+        /// <summary>
+        /// UT_Template Tablosuna Günceleme Yapan Fonksiyondur.
+        /// </summary>
+        /// <param name="item">UT_Template Objesidir. :Update Yapılacak Propertiler Eklenir</param>
+        /// <param name="setNull">Boş bırakılan propertiler null olarak mı setlensin bilgisi setlenir</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Update İşleminin Durumunu ve Başarılı Mesajını Geri Döndürür.</returns>
+        public ResultStatus UpdateUT_Template(UT_Template item, bool setNull = false, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteUpdate<UT_Template>(item, setNull);
+            }
+        }
+
+        /// <summary>
+        /// UT_Template tablosundan, Verilen kayıt id'si ile Silme İşlemi Yapan Fonksiyondur.
+        /// </summary>
+        /// <param name="id">UT_Template Tablo id'si</param>
+        /// <param name="tran">Mevcut Dışında Farklı Bir Transection Kullanılacak ise Bu Parametreye Gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Silme İşleminin Durumunu ve Başarılı Mesajını Geri Döndürür.</returns>
+        public ResultStatus DeleteUT_Template(Guid id, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteDelete<UT_Template>(id);
+            }
+        }
+
+        /// <summary>
+        /// UT_Template tablosundan, Verilen objeler ile Silme İşlemi Yapan Fonksiyondur.
+        /// </summary>
+        /// <param name="item">UT_Template Objesidir. Silme İşlemi Yapılacak Propertiler Eklenir, Eklenenkere Göre Filtrelenerek Silme işlemi yapılır.</param>
+        /// <param name="tran">Mevcut Dışında Farklı Bir Transection Kullanılacak ise Bu Parametreye Gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Silme İşleminin Durumunu ve Başarılı Mesajını Geri Döndürür.</returns>
+        public ResultStatus DeleteUT_Template(UT_Template item, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteDelete<UT_Template>(item);
+            }
+        }
+
+        /// <summary>
+        /// UT_Template Tablosuna Toplu insert işlemi yapan fonksiyondur.
+        /// </summary>
+        /// <param name="item">UT_Template Dizisi Gönderilerek insert işlemi yapılacak dizi objesi gönderilir.</param>
+        /// <param name="tran">Mevcut Dışında Farklı Bir Transection Kullanılacak ise Bu Parametreye Gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. insert işlemlerinin sonucunu ve başarılı mesajını geri döndürür.</returns>
+        public ResultStatus BulkInsertUT_Template(IEnumerable<UT_Template> item, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteBulkInsert<UT_Template>(item);
+            }
+        }
+
+        /// <summary>
+        /// UT_Template Tablosuna Toplu Update işlemi yapan fonksiyondur.
+        /// </summary>
+        /// <param name="item">UT_Template Dizisi Gönderilerek Update İşlemi Yapılacak Dizi Objesi Gönderilir.</param>
+        /// <param name="tran">Mevcut Dışında Farklı Bir Transection Kullanılacak ise Bu Parametreye Gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Update İşlemlerinin Sonucunu ve Başarılı Mesajını Geri Döndürür.</returns>
+        public ResultStatus BulkUpdateUT_Template(IEnumerable<UT_Template> item, bool setNull = false, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteBulkUpdate<UT_Template>(item, setNull);
+            }
+        }
+
+        /// <summary>
+        /// UT_Template tablosundan, Verilen Object List ile Toplu Silme İşlemi Yapan Fonksiyondur.
+        /// </summary>
+        /// <param name="item">UT_Template Dizisi Gönderilerek Delete işlemi yapılacak dizi objesi göderilir.</param>
+        /// <param name="tran">Mevcut Dışında Farklı Bir Transection Kullanılacak ise Bu Parametreye Gönderilir.</param>
+        /// <returns>ResultStatus Objesi Geri Döndürür. Delete işlemlerinin sonucunu ve başarılı mesajını geri döndürür.</returns>
+        public ResultStatus BulkDeleteUT_Template(IEnumerable<UT_Template> item, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.ExecuteBulkDelete<UT_Template>(item);
+            }
+        }
+
+    }
+}
---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/DatabaseFunctions/AutoGenerate/DBVWUT_Template.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infoline.Framework.Database;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using System.Data.Common;
+using Infoline.WorkOfTime.BusinessData;
+
+namespace Infoline.WorkOfTime.BusinessAccess
+{
+    partial class WorkOfTimeDatabase
+    {
+        /// <summary>
+        /// VWUT_Template tablosundan tüm kayıtları çeken fonksiyondur
+        /// </summary>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>VWUT_Template dizi objesini geri döndürür.</returns>
+        public VWUT_Template[] GetVWUT_Template(DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.Table<VWUT_Template>().OrderByDesc(a => a.created).Execute().ToArray();
+            }
+        }
+
+        /// <summary>
+        /// VWUT_Template tablosundan simpleQuery objesi filtresi sonucunda gelen kayıtları çeken fonksiyondur. Sayfa giridnde kullanılmaktadır.
+        /// </summary>
+        /// <param name="simpleQuery">Filtre parametreleri olarak obje doldurularak gönderilir.</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+         /// <returns>Filtre Sonucu VWUT_Template dizi objesini geri döndürür.</returns>
+        public VWUT_Template[] GetVWUT_Template(SimpleQuery simpleQuery, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+
+                return db.Table<VWUT_Template>().ExecuteSimpleQuery(simpleQuery).ToArray();
+            }
+        }
+        /// <summary>
+        /// VWUT_Template tablosundan BEXP Objesi filtresi sonucunda gelen kayıtların toplam adedini veren fonksiyondur.
+        /// </summary>
+        /// <param name="conditionExpression">Filtre parametreleri olarak obje doldurularak gönderilir.</param>
+        /// <returns>Filtre Sonucu Tablo adedini döndürür, sayı(int) olarak.</returns>
+        public int GetVWUT_TemplateCount(BEXP conditionExpression)
+        {
+            using (var db = GetDB())
+            {
+                return db.Table("VWUT_Template").Where(conditionExpression).Count();
+            }
+        }
+        /// <summary>
+        /// VWUT_Template tablosundan, id si gönderilen kaydın bilgilerini döndüren fonksiyondur.
+        /// </summary>
+        /// <param name="id">VWUT_Template tablo id'si verilir.</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>Filtre Sonucu VWUT_Template Objesini geri döndürür.</returns>
+        public VWUT_Template GetVWUT_TemplateById(Guid id, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+
+            {
+                return db.Table<VWUT_Template>().Where(a => a.id == id).Execute().FirstOrDefault();
+            }
+        }
+
+    }
+}
