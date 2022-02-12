0a28a548c836339bed30c5277b194cb6a2fa91c1
Okan Uregen <okan.uregen@infoline-tr.com>
2022-02-12 20:40:32 +0300

    VWSH_WorkAccidentCertificate ve VWSH_WorkAccidentCalendar viewları güncellendi

--- Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWSH_WorkAccidentCalendar.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWSH_WorkAccidentCalendar.cs
@@ -9,5 +9,11 @@ namespace Infoline.WorkOfTime.BusinessData
         public Guid? companyPersonCalendarId { get; set;}
         public string createdby_Title { get; set;}
         public string changedby_Title { get; set;}
+        public DateTime? startDate { get; set;}
+        public DateTime? endDate { get; set;}
+        public string title { get; set;}
+        public string description { get; set;}
+        public int? type { get; set;}
+        public string people_Title { get; set;}
     }
 }
--- Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWSH_WorkAccidentCertificate.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessData/DatabaseObjects/VWSH_WorkAccidentCertificate.cs
@@ -9,5 +9,15 @@ namespace Infoline.WorkOfTime.BusinessData
         public Guid? personCertificateId { get; set;}
         public string createdby_Title { get; set;}
         public string changedby_Title { get; set;}
+        public Guid? CertificateTypeId { get; set;}
+        public string CertificateTypeId_Title { get; set;}
+        public DateTime? startDate { get; set;}
+        public DateTime? endDate { get; set;}
+        public Guid? userId { get; set;}
+        public string userId_Title { get; set;}
+        public string certificateName { get; set;}
+        public int? certificateTime { get; set;}
+        public DateTime? expirationDate { get; set;}
+        public double? point { get; set;}
     }
 }
