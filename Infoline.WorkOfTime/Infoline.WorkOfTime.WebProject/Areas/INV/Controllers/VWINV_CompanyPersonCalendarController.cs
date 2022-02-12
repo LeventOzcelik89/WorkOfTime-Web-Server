52f97391d00e547d09aa3998b837ac6a13210c15
Okan Uregen <okan.uregen@infoline-tr.com>
2022-02-13 00:28:21 +0300

    workaccident update sayfsasından eğtğim ile ilgili crud işlemleri yaılabilir hale geldi

--- Infoline.WorkOfTime/Infoline.WorkOfTime.WebProject/Areas/INV/Controllers/VWINV_CompanyPersonCalendarController.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.WebProject/Areas/INV/Controllers/VWINV_CompanyPersonCalendarController.cs
@@ -55,6 +55,19 @@ namespace Infoline.WorkOfTime.WebProject.Areas.INV.Controllers
             return View(data);
         }
 
+        [AllowEveryone]
+        [PageInfo("Gündem Sayfası Detayı")]
+        public ActionResult DetailWorkAccident(Guid id)
+        {
+            var db = new WorkOfTimeDatabase();
+            var workAccident = db.GetVWSH_WorkAccidentCalendarById(id);
+            var data = db.GetVWINV_CompanyPersonCalendarById(workAccident.companyPersonCalendarId.Value);
+
+            var r = db.INV_CompanyPersonCalendarPersonsByIDPersonCalendarId(data.id).Select(s => s.IdUser).ToArray();
+            ViewBag.Persons = r;
+
+            return View(data);
+        }
 
         [PageInfo("Gündem Sayfası Veri Ekleme", SHRoles.Personel)]
         public ActionResult Insert(DateTime? StartDate, DateTime? EndDate)
@@ -65,11 +78,11 @@ namespace Infoline.WorkOfTime.WebProject.Areas.INV.Controllers
                 StartDate = StartDate,
                 EndDate = EndDate
             });
-        }        
-        
+        }
+
         [AllowEveryone]
         [PageInfo("İş Kazası Eğitim Sayfası Veri Ekleme")]
-        public ActionResult InsertWorkAccident(int Type,  Guid workAccidentId)
+        public ActionResult InsertWorkAccident(int Type, Guid workAccidentId)
         {
             ViewBag.workAccidentId = workAccidentId;
             return View(new VWINV_CompanyPersonCalendar
@@ -258,7 +271,6 @@ namespace Infoline.WorkOfTime.WebProject.Areas.INV.Controllers
         }
 
 
-
         [PageInfo("Gündem Sayfası Güncelleme", SHRoles.Personel)]
         public ActionResult Update(Guid id)
         {
@@ -343,7 +355,107 @@ namespace Infoline.WorkOfTime.WebProject.Areas.INV.Controllers
             return Json(new ResultStatusUI
             {
                 Result = false,
-                FeedBack = feedback.Success("Takvim Etkinliği güncelleme işlemi tamamlandır.\n" + item.Title)
+                FeedBack = feedback.Success("Takvim Etkinliği güncelleme işlemi tamamlandır.\n" + item.Title, false, Request.UrlReferrer.AbsoluteUri, timeout: 1)
+            }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [AllowEveryone]
+        [PageInfo("Gündem Sayfası Güncelleme")]
+        public ActionResult UpdateWorkAccident(Guid id)
+        {
+            var db = new WorkOfTimeDatabase();
+            var workAccident = db.GetVWSH_WorkAccidentCalendarById(id);
+            var data = db.GetVWINV_CompanyPersonCalendarById(workAccident.companyPersonCalendarId.Value);
+
+            var r = db.INV_CompanyPersonCalendarPersonsByIDPersonCalendarId(data.id).Select(s => s.IdUser).ToArray();
+            ViewBag.Persons = r;
+
+            return View(data);
+        }
+
+        [AllowEveryone]
+        [HttpPost, ValidateAntiForgeryToken]
+        [PageInfo("Gündem Sayfası Veri Güncelleme")]
+        public JsonResult UpdateWorkAccident(INV_CompanyPersonCalendar item, bool? mailForParticipants)
+        {
+            var db = new WorkOfTimeDatabase();
+
+            var control = db.GetINV_CompanyPersonCalendarById(item.id);
+            if(control == null)
+            {
+                var accident = db.GetSH_WorkAccidentCalendarById(item.id);
+                item.id = accident.companyPersonCalendarId.Value;
+            }
+            var userStatus = (PageSecurity)Session["userStatus"];
+            var feedback = new FeedBack();
+            item.changed = DateTime.Now;
+            item.changedby = userStatus.user.id;
+
+            var persons = Request["IdUsers"];
+            if (persons == null)
+            {
+                return Json(new ResultStatusUI
+                {
+                    Result = false,
+                    FeedBack = feedback.Warning("Herhangi bir personel seçimi yapılmadı.")
+                }, JsonRequestBehavior.AllowGet);
+            }
+
+            var personList = persons.Split(',').Select(a => new INV_CompanyPersonCalendarPersons
+            {
+                IdUser = a.ToGuid(),
+                created = DateTime.Now,
+                createdby = item.changedby,
+                IDPersonCalendar = item.id
+            }).ToArray();
+
+            var trans = db.BeginTransaction();
+
+            var tmpUsers = db.INV_CompanyPersonCalendarPersonsByIDPersonCalendarId(item.id);
+            var rsDelete = db.BulkDeleteINV_CompanyPersonCalendarPersons(tmpUsers, trans);
+
+            var dbres1 = db.UpdateINV_CompanyPersonCalendar(item, true, trans);
+            var dbres2 = db.BulkInsertINV_CompanyPersonCalendarPersons(personList, trans);
+
+            if (!dbres1.result || !dbres2.result)
+            {
+                trans.Rollback();
+                return Json(new ResultStatusUI
+                {
+                    Result = false,
+                    FeedBack = feedback.Error(dbres1.message + "\n" + dbres2.message)
+                }, JsonRequestBehavior.AllowGet);
+            }
+
+            if (mailForParticipants == true)
+            {
+                var stringType = ((EnumINV_CompanyPersonCalendarType)item.Type).ToDescription();
+                var emailUsers = string.Join(";", db.GetVWSH_UserByIds(persons.Split(',').Select(x => new Guid(x)).ToArray()).Select(x => x.email).ToArray());
+                if (item.Type == (Int32)EnumINV_CompanyPersonCalendarType.Toplanti)
+                {
+                    new Email().SendCalendar(emailUsers, item.Title, item.Description, item.StartDate.Value, item.EndDate.Value);
+                }
+                else
+                {
+                    var NewEndDate = item.Type == 100 || item.Type == 101 || item.Type == 103 || item.Type == 104 ? null : "- " + String.Format("{0:dd/MM/yyyy HH:mm}", item.EndDate.ToString());
+                    var tenantName = TenantConfig.Tenant.TenantName;
+                    var mesajIcerigi = string.Format(@"<h3>Merhaba,</h3> 
+                     <p>{0}  {1} tarihinde '{2}' etkinliği oluşturulmuştur. 
+                     </p> <p>{3}</p><p>Bilgilerinize.<br>İyi Çalışmalar.</p>",
+                          String.Format("{0:dd/MM/yyyy HH:mm}", item.StartDate), String.Format("{0:dd/MM/yyyy HH:mm}", NewEndDate), stringType, item.Description);
+
+                    new Email().Template("Template1", (item.Type == 106 ? 103 : item.Type) + ".jpg", item.Title, mesajIcerigi).Send((Int16)EmailSendTypes.DuyuruEtkinlik, emailUsers, string.Format("{0} | {1}", tenantName + " | WORKOFTIME", item.Title), true);
+                }
+            }
+
+            trans.Commit();
+            var rsFile = new FileUploadSave(Request, item.id).SaveAs();
+
+            return Json(new ResultStatusUI
+            {
+                Result = false,
+                FeedBack = feedback.Success("Takvim Etkinliği güncelleme işlemi tamamlandır.\n" + item.Title, false, Request.UrlReferrer.AbsoluteUri, timeout: 1)
             }, JsonRequestBehavior.AllowGet);
 
         }
