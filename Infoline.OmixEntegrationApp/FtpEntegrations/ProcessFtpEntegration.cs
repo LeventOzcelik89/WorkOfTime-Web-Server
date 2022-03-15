71224af56dce5db749b3e011cc88b38e4f1ade29
Ömer DURSUN <omer.dursun@infoline-tr.com>
2022-03-15 09:50:07 +0300

    ajan test için düzenlendi.

--- Infoline.OmixEntegrationApp/FtpEntegrations/ProcessFtpEntegration.cs
+++ Infoline.OmixEntegrationApp/FtpEntegrations/ProcessFtpEntegration.cs
@@ -18,58 +18,86 @@ namespace Infoline.OmixEntegrationApp.FtpEntegrations
         }
         public void Run()
         {
-            if (task == null)
+            //if (task == null)
+            //{
+            //    lock (control)
+            //    {
+            //        task = System.Threading.Tasks.Task.Run(() =>
+            //        {
+            //            do
+            //            {
+            //                try
+            //                {
+            //                    var now = DateTime.Now;
+            //                    var db = new WorkOfTimeDatabase();
+            //                    if (now.Hour == 1 && now.Minute == 00)
+            //                    {
+            //                        var taskMobitel = new Task(() =>
+            //                        {
+            //                        new FtpMobitel().ExportFilesToDatabase(); ;
+            //                        });
+            //                        Tasks.Add(taskMobitel);
+
+            //                        var taskGenpa = new Task(() =>
+            //                        {
+            //                            new FtpGenpa().ExportFilesToDatabase(); ;
+            //                        });
+            //                        Tasks.Add(taskGenpa);
+
+            //                        var taskKvk = new Task(() =>
+            //                        {
+            //                            new FtpKvk().ExportFilesToDatabase(); ;
+            //                        });
+            //                        Tasks.Add(taskKvk);
+
+            //                        var taskPort = new Task(() =>
+            //                        {
+            //                            new FtpPort().ExportFilesToDatabase(); ;
+            //                        });
+            //                        Tasks.Add(taskPort);
+
+            //                        foreach (var task in Tasks)
+            //                        {
+            //                            task.Start();
+            //                        }
+            //                    }
+            //                }
+            //                catch (Exception ex)
+            //                {
+            //                    Log.Error("Entegrasyon servisleri çalışırken sorun oluştu. Mesaj : " + ex.Message + "mm: " + ex.InnerException.Message);
+            //                }
+            //                System.Threading.Thread.Sleep(new TimeSpan(0, 1, 0));
+            //            } while (true);
+            //        });
+            //    }
+            //}
+            var taskMobitel = new Task(() =>
             {
-                lock (control)
-                {
-                    task = System.Threading.Tasks.Task.Run(() =>
-                    {
-                        do
-                        {
-                            try
-                            {
-                                var now = DateTime.Now;
-                                var db = new WorkOfTimeDatabase();
-                                if (now.Hour == 1 && now.Minute == 00)
-                                {
-                                    var taskMobitel = new Task(() =>
-                                {
-                                    new FtpMobitel().ExportFilesToDatabase(); ;
-                                });
-                                    Tasks.Add(taskMobitel);
+                new FtpMobitel().ExportFilesToDatabase(); ;
+            });
+            Tasks.Add(taskMobitel);
 
-                                    var taskGenpa = new Task(() =>
-                                    {
-                                        new FtpGenpa().ExportFilesToDatabase(); ;
-                                    });
-                                    Tasks.Add(taskGenpa);
+            var taskGenpa = new Task(() =>
+            {
+                new FtpGenpa().ExportFilesToDatabase(); ;
+            });
+            Tasks.Add(taskGenpa);
 
-                                    var taskKvk = new Task(() =>
-                                    {
-                                        new FtpKvk().ExportFilesToDatabase(); ;
-                                    });
-                                    Tasks.Add(taskKvk);
+            var taskKvk = new Task(() =>
+            {
+                new FtpKvk().ExportFilesToDatabase(); ;
+            });
+            Tasks.Add(taskKvk);
 
-                                    var taskPort = new Task(() =>
-                                    {
-                                        new FtpPort().ExportFilesToDatabase(); ;
-                                    });
-                                    Tasks.Add(taskPort);
+            var taskPort = new Task(() =>
+            {
+                new FtpPort().ExportFilesToDatabase(); ;
+            });
+            Tasks.Add(taskPort);
 
-                                    foreach (var task in Tasks)
-                                    {
-                                        task.Start();
-                                    }
-                                }
-                            }
-                            catch (Exception ex)
-                            {
-                                Log.Error("Entegrasyon servisleri çalışırken sorun oluştu. Mesaj : " + ex.Message + "mm: " + ex.InnerException.Message);
-                            }
-                            System.Threading.Thread.Sleep(new TimeSpan(0, 1, 0));
-                        } while (true);
-                    });
-                }
+            foreach (var task in Tasks)
+            {
+                task.Start();
             }
         }
         public void Dispose()
