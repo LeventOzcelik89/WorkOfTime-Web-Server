798a3a6830db673db9d852cf27101ebf66a93016
Ömer DURSUN <omer.dursun@infoline-tr.com>
2022-03-17 14:52:57 +0300

    Hakediş Sayfalarının yetkileri ayarlandı.

--- Infoline.WorkOfTime/Infoline.WorkOfTime.WebProject/Areas/PRD/Controllers/VWPRD_ProductPaymentController.cs
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.WebProject/Areas/PRD/Controllers/VWPRD_ProductPaymentController.cs
@@ -13,12 +13,13 @@ namespace Infoline.WorkOfTime.WebProject.Areas.PRD.Controllers
 {
 	public class VWPRD_ProductPaymentController : Controller
 	{
+		[PageInfo("Hakediş Raporu", SHRoles.SistemYonetici)]
 		public ActionResult Index()
 		{
 		    return View();
 		}
 
-
+		[PageInfo("Hakediş Raporu Veri Kaynağı", SHRoles.SistemYonetici)]
 		public ContentResult DataSource([DataSourceRequest]DataSourceRequest request)
 		{
 		    var condition = KendoToExpression.Convert(request);
@@ -33,100 +34,12 @@ namespace Infoline.WorkOfTime.WebProject.Areas.PRD.Controllers
 		    return Content(Infoline.Helper.Json.Serialize(data), "application/json");
 		}
 
-
-		public ContentResult DataSourceDropDown([DataSourceRequest]DataSourceRequest request)
-		{
-		    var condition = KendoToExpression.Convert(request);
-
-		    var db = new WorkOfTimeDatabase();
-		    var data = db.GetVWPRD_ProductPayment(condition);
-		    return Content(Infoline.Helper.Json.Serialize(data), "application/json");
-		}
-
-
+		[PageInfo("Hakediş Raporu Detay", SHRoles.SistemYonetici)]
 		public ActionResult Detail(Guid id)
 		{
 		    var db = new WorkOfTimeDatabase();
 		    var data = db.GetVWPRD_ProductPaymentById(id);
 		    return View(data);
 		}
-
-
-		public ActionResult Insert()
-		{
-		    var data = new VWPRD_ProductPayment { id = Guid.NewGuid() };
-		    return View(data);
-		}
-
-
-		[HttpPost, ValidateAntiForgeryToken]
-		public JsonResult Insert(PRD_ProductPayment item)
-		{
-		    var db = new WorkOfTimeDatabase();
-		    var userStatus = (PageSecurity)Session["userStatus"];
-		    var feedback = new FeedBack();
-		    item.created = DateTime.Now;
-		    item.createdby = userStatus.user.id;
-		    var dbresult = db.InsertPRD_ProductPayment(item);
-		    var result = new ResultStatusUI
-		    {
-		        Result = dbresult.result,
-		        FeedBack = dbresult.result ? feedback.Success("Kaydetme işlemi başarılı") : feedback.Error("Kaydetme işlemi başarısız")
-		    };
-		
-		    return Json(result, JsonRequestBehavior.AllowGet);
-		}
-
-
-		public ActionResult Update(Guid id)
-		{
-		    var db = new WorkOfTimeDatabase();
-		    var data = db.GetVWPRD_ProductPaymentById(id);
-		    return View(data);
-		}
-
-
-		[HttpPost, ValidateAntiForgeryToken]
-		public JsonResult Update(PRD_ProductPayment item)
-		{
-		    var db = new WorkOfTimeDatabase();
-		    var userStatus = (PageSecurity)Session["userStatus"];
-		    var feedback = new FeedBack();
-		
-		    item.changed = DateTime.Now;
-		    item.changedby = userStatus.user.id;
-		
-		    var dbresult = db.UpdatePRD_ProductPayment(item);
-		    var result = new ResultStatusUI
-		    {
-		        Result = dbresult.result,
-		        FeedBack = dbresult.result ? feedback.Success("Güncelleme işlemi başarılı") : feedback.Error("Güncelleme işlemi başarısız")
-		    };
-		
-		    return Json(result, JsonRequestBehavior.AllowGet);
-		}
-
-
-		[HttpPost]
-		public JsonResult Delete(string[] id)
-		{
-		    var db = new WorkOfTimeDatabase();
-		    var feedback = new FeedBack();
-		
-		    var item = id.Select(a => new PRD_ProductPayment { id = new Guid(a) });
-		
-		    var dbresult = db.BulkDeletePRD_ProductPayment(item);
-		
-		    var result = new ResultStatusUI
-		    {
-		        Result = dbresult.result,
-		        FeedBack = dbresult.result ? feedback.Success("Silme işlemi başarılı") : feedback.Error("Silme işlemi başarılı")
-		    };
-		
-		    return Json(result, JsonRequestBehavior.AllowGet);
-		}
-
-
-
 	}
 }
