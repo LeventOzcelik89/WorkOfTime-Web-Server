ee8a5bb64fddcada67adda0720085cee07b1bbe7
Yunus Emre ŞANLIBAYRAK <yunus.sanlibayrak@infoline-tr.com>
2022-03-25 19:26:34 +0300

    VWFVR_Favorites Model Controller ve view eklendi üzerinde çalışılıyor.

---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/Business/FVR/VWFVR_FavoritesModel.cs
@@ -0,0 +1,163 @@
+﻿using Infoline.Framework.Database;
+using Infoline.WorkOfTime.BusinessData;
+using Infoline.WorkOfTime.BusinessData.Specific;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Infoline.WorkOfTime.BusinessAccess.Business
+{
+    public class VWFVR_FavoritesModel : VWFVR_Favorites
+    {
+        private WorkOfTimeDatabase db { get; set; }
+        private DbTransaction trans { get; set; }
+        public VWFVR_FavoritesModel Load()
+        {
+            this.db = this.db ?? new WorkOfTimeDatabase();
+            var data = db.GetVWFVR_FavoritesById(this.id);
+            if (data != null)
+            {
+                this.B_EntityDataCopyForMaterial(data, true);
+            }
+            return this;
+        }
+        public ResultStatus Save(Guid? userId = null, HttpRequestBase request = null, DbTransaction transaction = null)
+        {
+            db = db ?? new WorkOfTimeDatabase();
+            trans = transaction ?? db.BeginTransaction();
+            var data = db.GetVWFVR_FavoritesById(this.id);
+            var res = new ResultStatus { result = true };
+            var validation = Validator();
+            if (validation.result == false)
+            {
+                return validation;
+            }
+            if (data == null)
+            {
+                this.createdby = userId;
+                this.created = DateTime.Now;
+                res = Insert();
+            }
+            else
+            {
+                this.changedby = userId;
+                this.changed = DateTime.Now;
+                res = Update();
+            }
+            if (res.result)
+            {
+                if (transaction == null) trans.Commit();
+            }
+            else
+            {
+                if (transaction == null) trans.Rollback();
+            }
+            return res;
+        }
+        private ResultStatus Validator()
+        {
+            db = db ?? new WorkOfTimeDatabase();
+            var res = new ResultStatus { result = true };
+            return res;
+        }
+        private ResultStatus Insert()
+        {
+            db = db ?? new WorkOfTimeDatabase();
+            //Validasyonlarını yap
+            var dbresult = db.InsertFVR_Favorites(new FVR_Favorites
+            {
+                userId = createdby
+            }.B_EntityDataCopyForMaterial(this), this.trans);
+            if (!dbresult.result)
+            {
+                Log.Error(dbresult.message);
+                return new ResultStatus
+                {
+                    result = false,
+                    message = "Favori oluşturma işlemi başarısız oldu."
+                };
+            }
+            else
+            {
+                return new ResultStatus
+                {
+                    result = true,
+                    message = "Favori oluşturma işlemi başarılı şekilde gerçekleştirildi."
+                };
+            }
+        }
+        private ResultStatus Update()
+        {
+            var dbresult = new ResultStatus { result = true };
+            dbresult &= db.UpdateFVR_Favorites(new FVR_Favorites().B_EntityDataCopyForMaterial(this), false, this.trans);
+            if (!dbresult.result)
+            {
+                Log.Error(dbresult.message);
+                return new ResultStatus
+                {
+                    result = false,
+                    message = "Favori güncelleme işlemi başarısız oldu."
+                };
+            }
+            else
+            {
+                return new ResultStatus
+                {
+                    result = true,
+                    message = "Favori güncelleme işlemi başarılı şekilde gerçekleştirildi."
+                };
+            }
+        }
+        public ResultStatus Delete(DbTransaction transaction = null)
+        {
+            db = db ?? new WorkOfTimeDatabase();
+            trans = transaction ?? db.BeginTransaction();
+            //İlişkili kayıtlar kontol edilerek dilme işlemine müsade edilecek;
+            var dbresult = db.DeleteFVR_Favorites(new FVR_Favorites { id = this.id }, trans);
+            if (!dbresult.result)
+            {
+                if (transaction == null) trans.Rollback();
+                return new ResultStatus
+                {
+                    result = false,
+                    message = "Favori silme işlemi başarısız oldu."
+                };
+            }
+            else
+            {
+                if (transaction == null) trans.Commit();
+                return new ResultStatus
+                {
+                    result = true,
+                    message = "Favori silme işlemi başarılı şekilde gerçekleştirildi."
+                };
+            }
+        }
+        public static ResultStatus Import()
+        {
+            return null;
+        }
+        public static PageModel GetPageModel()
+        {
+            var pageModel = new PageModel { Title = "Favori" };
+            return pageModel;
+        }
+        public static SimpleQuery UpdateDataSourceFilter(SimpleQuery query, PageSecurity userStatus)
+        {
+            BEXP filter = null;
+            filter = new BEXP
+            {
+                Operand1 = (COL)"userId",
+                Operator = BinaryOperator.Equal,
+                Operand2 = (VAL)userStatus.user.id
+
+            };
+            query.Filter &= filter;
+            return query;
+        }
+    }
+}
---
+++ Infoline.WorkOfTime/Infoline.WorkOfTime.BusinessAccess/DatabaseFunctions/AutoGenerate/DBVWFVR_Favorites.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infoline.Framework.Database;
+using System.Data.SqlClient;
+using System.Linq.Expressions;
+using System.Data.Common;
+using Infoline.WorkOfTime.BusinessData;
+
+namespace Infoline.WorkOfTime.BusinessAccess
+{
+    partial class WorkOfTimeDatabase
+    {
+        /// <summary>
+        /// VWFVR_Favorites tablosundan tüm kayıtları çeken fonksiyondur
+        /// </summary>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>VWFVR_Favorites dizi objesini geri döndürür.</returns>
+        public VWFVR_Favorites[] GetVWFVR_Favorites(DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+                return db.Table<VWFVR_Favorites>().OrderByDesc(a => a.created).Execute().ToArray();
+            }
+        }
+
+        /// <summary>
+        /// VWFVR_Favorites tablosundan simpleQuery objesi filtresi sonucunda gelen kayıtları çeken fonksiyondur. Sayfa giridnde kullanılmaktadır.
+        /// </summary>
+        /// <param name="simpleQuery">Filtre parametreleri olarak obje doldurularak gönderilir.</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+         /// <returns>Filtre Sonucu VWFVR_Favorites dizi objesini geri döndürür.</returns>
+        public VWFVR_Favorites[] GetVWFVR_Favorites(SimpleQuery simpleQuery, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+            {
+
+                return db.Table<VWFVR_Favorites>().ExecuteSimpleQuery(simpleQuery).ToArray();
+            }
+        }
+        /// <summary>
+        /// VWFVR_Favorites tablosundan BEXP Objesi filtresi sonucunda gelen kayıtların toplam adedini veren fonksiyondur.
+        /// </summary>
+        /// <param name="conditionExpression">Filtre parametreleri olarak obje doldurularak gönderilir.</param>
+        /// <returns>Filtre Sonucu Tablo adedini döndürür, sayı(int) olarak.</returns>
+        public int GetVWFVR_FavoritesCount(BEXP conditionExpression)
+        {
+            using (var db = GetDB())
+            {
+                return db.Table("VWFVR_Favorites").Where(conditionExpression).Count();
+            }
+        }
+        /// <summary>
+        /// VWFVR_Favorites tablosundan, id si gönderilen kaydın bilgilerini döndüren fonksiyondur.
+        /// </summary>
+        /// <param name="id">VWFVR_Favorites tablo id'si verilir.</param>
+        /// <param name="tran">Mevcut dışında farklı bir transection kullanılacak ise bu parametreye gönderilir.</param>
+        /// <returns>Filtre Sonucu VWFVR_Favorites Objesini geri döndürür.</returns>
+        public VWFVR_Favorites GetVWFVR_FavoritesById(Guid id, DbTransaction tran = null)
+        {
+            using (var db = GetDB(tran))
+
+            {
+                return db.Table<VWFVR_Favorites>().Where(a => a.id == id).Execute().FirstOrDefault();
+            }
+        }
+
+    }
+}
